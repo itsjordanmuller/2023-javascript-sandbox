45a049fc5a08839929e69e1d5f332e8318029e71
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 16:40:18 -0700

    Copy previous console exercises to comments & shortcuts lesson

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+console.log(100);
+
+console.log(20, "Hello", true);
+
+const x = 500;
+console.log(x);
+
+console.group("Simple");
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
+
+console.table({ name: "Jordan", email: "itsjordanmuller@gmail.com" });
+
+const styles = "padding: 10px; background-color: white; color: green";
+
+console.log("%cStyled Console Text", styles);
