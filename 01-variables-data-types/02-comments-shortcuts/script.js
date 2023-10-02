cfb8fccae2991d02da93f822ec2061989d9b506f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 16:43:56 -0700

    Create a single and a multiple line comment in script file

--- 01-variables-data-types/02-comments-shortcuts/script.js
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -1,3 +1,6 @@
+// This is a Single-Line Comment
+
+/* This is a Multi-Line Comment
 console.log("Hello World!");
 
 console.log(100);
@@ -6,6 +9,7 @@ console.log(20, "Hello", true);
 
 const x = 500;
 console.log(x);
+*/
 
 console.group("Simple");
 console.error("Alert");
