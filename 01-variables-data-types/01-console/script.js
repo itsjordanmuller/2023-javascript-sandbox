29503c7b72e13abddb956a9e410007af17fd2a66
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 16:35:18 -0700

    Wrap the error and warning console messages into a group and end the group to avoid wrapping all subsequent console actions

--- 01-variables-data-types/01-console/script.js
+++ 01-variables-data-types/01-console/script.js
@@ -7,7 +7,9 @@ console.log(20, "Hello", true);
 const x = 500;
 console.log(x);
 
+console.group("Simple");
 console.error("Alert");
 console.warn("Warning");
+console.groupEnd();
 
 console.table({ name: "Jordan", email: "itsjordanmuller@gmail.com" });
