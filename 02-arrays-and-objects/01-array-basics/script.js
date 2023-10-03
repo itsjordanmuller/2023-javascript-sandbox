483b5445abe4a467753f4197256fc8cb1d92f709
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 16:51:55 -0700

    Interact with specific index values of the arrays

--- 02-arrays-and-objects/01-array-basics/script.js
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -4,4 +4,9 @@ const numbers = [10, 20, 30, 40, 50];
 // Array Constructor
 const fruits = new Array("Apple", "Watermelon", "Orange");
 
-console.log(fruits);
+// Value at a Specific Index
+x = numbers[0];
+x = numbers[0] + numbers[3];
+x = `My favorite fruit is the: ${fruits[1]}`;
+
+console.log(x);
