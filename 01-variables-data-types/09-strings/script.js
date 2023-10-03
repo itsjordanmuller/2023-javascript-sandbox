837a9c46dc1ace009fa89a2d6e180ef89cd8cea3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 18:03:20 -0700

    Demonstrate a huge variety of methods that can be used on a string to do various actions to it

--- 01-variables-data-types/09-strings/script.js
+++ 01-variables-data-types/09-strings/script.js
@@ -14,4 +14,31 @@ const s = new String("Hello World!");
 x = typeof s;
 x = s.length;
 
+// Access Value by Key
+x = s[0];
+
+x = s.__proto__;
+
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+x = s.charAt(0);
+
+x = s.indexOf("d");
+
+x = s.substring(0, 5);
+x = s.substring(6);
+
+x = s.slice(-12, -7);
+
+x = x.trim();
+
+x = s.replace("World", "Jordan");
+
+x = s.includes("Hello");
+
+x = s.valueOf();
+
+x = s.split(" ");
+
 console.log(x);
