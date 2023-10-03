742ec164928dae1c2b0ebc3ae521fcca6cef2bea
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 18:10:17 -0700

    Complete capitalization challenge by logging title case string to console

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,9 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = "developer";
+
+console.log(myString);
+
+const myNewString =
+  myString[0].toUpperCase() + myString.slice(1, myString.length);
+
+console.log(myNewString);
