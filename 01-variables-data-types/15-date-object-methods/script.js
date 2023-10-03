76e6a5d729567f1a9482387538ba8766fdd3300e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 18:38:47 -0700

    Showcase international date time formats and to locale string methods for date objects

--- 01-variables-data-types/15-date-object-methods/script.js
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -17,4 +17,23 @@ x = d.getMilliseconds();
 
 x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 
+x = Intl.DateTimeFormat("en-US").format(d);
+x = Intl.DateTimeFormat("en-GB").format(d);
+x = Intl.DateTimeFormat("default").format(d);
+
+x = Intl.DateTimeFormat("default", { month: "long" }).format(d);
+
+x = d.toLocaleString("default", { month: "short" });
+
+x = d.toLocaleString("default", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  timeZone: "America/Los_Angeles",
+});
+
 console.log(x);
