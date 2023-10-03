db40f871467b2d51eb144f53ff7f7a4648cbfbf0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 17:24:52 -0700

    Demonstrate how constants pointing to the same value are changed together as one in the heap

--- 01-variables-data-types/05-stack-heap/script.js
+++ 01-variables-data-types/05-stack-heap/script.js
@@ -1,3 +1,18 @@
 // Values are Stored on the Stack
 const name = "Jordan";
 const age = 23;
+
+// Reference Values are Stored on the Heap
+const person = {
+  name: "John",
+  age: 23,
+};
+
+let newName = name;
+newName = "Jordan";
+
+let newPerson = person;
+newPerson.name = "Jordan";
+
+console.log(name, newName);
+console.log(person, newPerson);
