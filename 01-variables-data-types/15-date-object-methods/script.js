81a165330b0f053d98a97959ba87e670d180bbef
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 18:35:03 -0700

    Get various parts of the date, and log date to console using template literal formatting

--- 01-variables-data-types/15-date-object-methods/script.js
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -5,5 +5,16 @@ x = d.getTime();
 x = d.valueOf();
 
 x = d.getFullYear();
+x = d.getMonth() + 1;
+x = d.getDate();
+
+x = d.getDay();
+
+x = d.getHours();
+x = d.getMinutes();
+x = d.getSeconds();
+x = d.getMilliseconds();
+
+x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 
 console.log(x);
