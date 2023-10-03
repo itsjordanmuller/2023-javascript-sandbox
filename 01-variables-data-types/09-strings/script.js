85773c8535866dbd0a6e9d626ea341f1442efa73
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 17:53:52 -0700

    Use a template literal to concatenate strings in an easier way

--- 01-variables-data-types/09-strings/script.js
+++ 01-variables-data-types/09-strings/script.js
@@ -3,6 +3,9 @@ let x;
 const name = "Jordan";
 const age = 23;
 
-x = "Hello, my name is " + name + " and I am " + age + " years old";
+// x = "Hello, my name is " + name + " and I am " + age + " years old";
+
+// Template Literals
+x = `Hello, my name is ${name} and I am ${age} years old`;
 
 console.log(x);
