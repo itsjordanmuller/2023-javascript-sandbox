cdef0451b29e359e1234f556db0cdcd72c0e5c4e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 17:56:27 -0700

    Use and demonstrate string properties and methods

--- 01-variables-data-types/09-strings/script.js
+++ 01-variables-data-types/09-strings/script.js
@@ -8,4 +8,10 @@ const age = 23;
 // Template Literals
 x = `Hello, my name is ${name} and I am ${age} years old`;
 
+// String Properties and Methods
+const s = new String("Hello World!");
+
+x = typeof s;
+x = s.length;
+
 console.log(x);
