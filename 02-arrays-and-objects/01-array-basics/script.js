b73bdda9b6ce9bda4768f634d3d470d296bb95da
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 16:55:07 -0700

    Get length, set value at index, and cut off array at specific length

--- 02-arrays-and-objects/01-array-basics/script.js
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -9,4 +9,14 @@ x = numbers[0];
 x = numbers[0] + numbers[3];
 x = `My favorite fruit is the: ${fruits[1]}`;
 
+// Get Length of an Array
+x = numbers.length;
+
+// Set Value at a Specific Index in an Array
+fruits[2] = "Strawberry";
+x = fruits;
+
+// Set Length to Cut Off Array
+// fruits.length = 2;
+
 console.log(x);
