613543dd50dec519549437f188c85ac64d76c771
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-02 17:30:24 -0700

    Show multiple ways that a number can be converted to a type of string

--- 01-variables-data-types/06-type-conversion/script.js
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -1,8 +1,12 @@
-let amount = "100";
+let amount = 100;
 
 // Change String to Number
 // amount = parseInt(amount);
 // amount = +amount;
 // amount = Number(amount);
+
+// Change a Number to a String
+// amount = amount.toString();
+// amount = String(amount);
 
 console.log(amount, typeof amount);
