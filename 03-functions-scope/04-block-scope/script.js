1bea5ca37946cfd4d9aa1e73a313b84f411e9a8a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 21:54:43 -0700

    Show various situations where block level scope applies with var and other situations

--- 03-functions-scope/04-block-scope/script.js
+++ 03-functions-scope/04-block-scope/script.js
@@ -7,3 +7,36 @@ if (true) {
 
 // Reference Error: y is not defined at this scope
 // console.log(x + y);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// Reference Error: i is not defined at this scope
+// console.log(i);
+
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+// Accessible When var is Used at a Block Level
+console.log(c);
+
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+// Not Accessible When var is Used at a Function Level
+// Reference Error: d is not defined at this scope
+// console.log(d);
+
+const foo = 1;
+var bar = 2;
+
+// Const is Not Accessible from Window Object
+console.log(window.foo);
+// Var Is Accessible from Window Object
+console.log(window.bar);
