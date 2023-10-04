5d43da5eace6fa5e14e3d845463f782959c682e9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 18:14:55 -0700

    Stringify an array of objects into JSON string

--- 02-arrays-and-objects/08-json-intro/script.js
+++ 02-arrays-and-objects/08-json-intro/script.js
@@ -12,4 +12,21 @@ x = post;
 const obj = JSON.parse(str);
 x = obj;
 
+const posts = [
+  {
+    id: 1,
+    title: "Post One",
+    body: "This is the body of the post!",
+  },
+  {
+    id: 2,
+    title: "Post Two",
+    body: "This is the body of the post!",
+  },
+];
+x = posts;
+
+const str2 = JSON.stringify(posts);
+x = str2;
+
 console.log(x);
