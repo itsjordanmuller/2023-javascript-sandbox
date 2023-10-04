fb02c5ce80328d6c51f3145656df259fa4601952
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:06:35 -0700

    Pass an index, array, and function into forEach to show different situations

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -13,4 +13,16 @@ const socials = ["LinkedIn", "Facebook", "Instagram", "TikTok"];
 // });
 
 // Even Shorter Version with No Curly Braces
-socials.forEach((social) => console.log(social));
+// socials.forEach((social) => console.log(social));
+
+// Pass Index and Array into forEach
+// socials.forEach((social, index, arr) =>
+//   console.log(`${index} - ${social}`, arr)
+// );
+
+// Pass a Function into forEach
+function logSocials(social) {
+  console.log(social);
+}
+
+socials.forEach(logSocials);
