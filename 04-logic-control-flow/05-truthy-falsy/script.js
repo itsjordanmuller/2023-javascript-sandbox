e63260dbfce35d9f840e3670ea38cd938578427c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 12:36:10 -0700

    Show more examples of truthy values that are commonly mistaken as falsy

--- 04-logic-control-flow/05-truthy-falsy/script.js
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -6,6 +6,24 @@
 // - undefined
 // - NaN
 
+const x = false;
+
+if (x) {
+  console.log("This is truthy");
+} else {
+  console.log("This is falsy");
+}
+
+// Truthy Values:
+// - Everything else that is not falsy
+// - true
+// - "0" (0 as a string)
+// - " " (Space in a String)
+// - "false" (False in a String)
+// - [] (Empty Array)
+// - {} (Empty Object)
+// - function () {} (Empty Function)
+
 const email = "test@test.com";
 
 if (email) {
