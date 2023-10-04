b8e762086eee2f9d8a02cf49344021bbdf9e1882
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 21:45:02 -0700

    Show different scopes including global, block, and function scope

--- 03-functions-scope/03-global-function-scope/script.js
+++ 03-functions-scope/03-global-function-scope/script.js
@@ -2,10 +2,24 @@
 // window.alert("Hello");
 // alert("World!");
 
+const x = 100;
+
 // Function with Window Properties
 function run() {
   console.log(window.innerWidth);
   console.log(innerWidth);
+  // Function Scope
+  const y = 50;
+  console.log(y, "in function");
 }
 
 run();
+
+// Global Scope
+console.log(x, "in global");
+
+// Block Scope
+if (true) {
+  const z = 75;
+  console.log(z, "in block");
+}
