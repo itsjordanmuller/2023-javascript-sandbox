f37349b30b3fabf83960efe18d683275b4627d2d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:19:28 -0700

    Get only the companies with a category of retail using a filter

--- 05-iteration-array-methods/08-filter/script.js
+++ 05-iteration-array-methods/08-filter/script.js
@@ -1,4 +1,4 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Default Way of Writing Filter with Function
 // const evenNumbers = numbers.filter(function (number) {
@@ -9,11 +9,29 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
 // Same But Much Longer with forEach
-const evenNumbers = [];
-numbers.forEach((number) => {
-  if (number % 2 === 0) {
-    evenNumbers.push(number);
-  }
-});
+// const evenNumbers = [];
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     evenNumbers.push(number);
+//   }
+// });
 
-console.log(evenNumbers);
+// console.log(evenNumbers);
+
+const companies = [
+  { name: "Company 1", category: "Finance", start: 1981, end: 2004 },
+  { name: "Company 2", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company 3", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company 4", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company 5", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company 6", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company 7", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company 8", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company 9", category: "Retail", start: 1981, end: 1989 },
+];
+
+// Get Only Retail Companies
+const retailCompanies = companies.filter(
+  (company) => company.category === "Retail"
+);
+console.log(retailCompanies);
