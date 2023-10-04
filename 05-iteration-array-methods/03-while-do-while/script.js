ab80ac34a71a63ccd5994b749f68ddd3f0bf9424
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:41:29 -0700

    Demonstrate the use of a do while loop that does an action once regardless of if the while is true

--- 05-iteration-array-methods/03-while-do-while/script.js
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -15,16 +15,23 @@
 // }
 
 // Nesting While Loops
-let i = 1;
+// let i = 1;
 
-while (i <= 5) {
+// while (i <= 5) {
+//   console.log(`Number: ${i}`);
+
+//   let j = 1;
+//   while (j <= 5) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//     j++;
+//   }
+
+//   i++;
+// }
+
+// Do While Loop (Code in Do Is Executed Once No Matter What, Then Again While True)
+let i = 21;
+do {
   console.log(`Number: ${i}`);
-
-  let j = 1;
-  while (j <= 5) {
-    console.log(`${i} * ${j} = ${i * j}`);
-    j++;
-  }
-
   i++;
-}
+} while (i <= 20);
