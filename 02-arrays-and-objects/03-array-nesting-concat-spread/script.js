a3bd06976eec59a693c05c9d98f65e2d67150467
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:22:23 -0700

    Test out a variety of different static methods that can be used on the Array object

--- 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
+++ 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
@@ -25,4 +25,19 @@ x = [...fruits, ...berries];
 const arr = [1, 2, [3, 4], 5, [6, 7], 8];
 x = arr.flat();
 
+// Static Methods on Array Objects
+
+// Check if Something is an Array
+x = Array.isArray("Hello");
+
+// Create an Array from an Array Like Value
+x = Array.from("12345");
+
+const a = 1;
+const b = 2;
+const c = 3;
+
+// Another Way of Creating Arrays from Values
+x = Array.of(a, b, c);
+
 console.log(x);
