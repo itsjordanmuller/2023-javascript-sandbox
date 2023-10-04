0d3779562be1de3d7dc974292aea6d73757842c5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:33:19 -0700

    Log the entire object, and then a specific property from the object to the console

---
+++ 02-arrays-and-objects/05-object-literals/script.js
@@ -0,0 +1,12 @@
+// Create an Object
+const person = {
+  name: "Jordan",
+  age: 23,
+  isAdmin: true,
+};
+
+// Log Entire Object
+console.log(person);
+
+// Log Specific Property from an Object
+console.log(person.name);
