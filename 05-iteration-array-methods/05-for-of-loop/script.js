630c74f4568266a5598a8e90cf33e96e52444aaf
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:51:14 -0700

    Show how a for of loop can be used to more easily loop through arrays

--- 05-iteration-array-methods/05-for-of-loop/script.js
+++ 05-iteration-array-methods/05-for-of-loop/script.js
@@ -1,6 +1,11 @@
 const items = ["Book", "Table", "Chair", "Kite"];
 
 // Default Way to Loop Through Array with For Loop
-for (let i = 0; i < items.length; i++) {
-  console.log(items[i]);
+// for (let i = 0; i < items.length; i++) {
+//   console.log(items[i]);
+// }
+
+// Does the Same Thing, But Shorter
+for (const item of items) {
+  console.log(item);
 }
