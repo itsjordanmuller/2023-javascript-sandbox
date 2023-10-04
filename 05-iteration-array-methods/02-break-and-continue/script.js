874252d705ac1dc94e38206778d6eaac98a90bb9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:33:22 -0700

    Use continue to skip 13 and move through the rest of the loop

--- 05-iteration-array-methods/02-break-and-continue/script.js
+++ 05-iteration-array-methods/02-break-and-continue/script.js
@@ -1,8 +1,18 @@
 // Break Out of a Loop
+// for (let i = 0; i <= 20; i++) {
+//   if (i === 15) {
+//     console.log("Breaking...");
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// Continue to Skip Current Iteration and Move to Next Iteration
 for (let i = 0; i <= 20; i++) {
-  if (i === 15) {
-    console.log("Breaking...");
-    break;
+  if (i === 13) {
+    console.log("Skipping 13...");
+    continue;
   }
+
   console.log(i);
 }
