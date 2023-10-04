4184f34d9630c5617f45b0c301d96c92dc6a12ce
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:50:29 -0700

    Use the reduce method to reduce an array of objects with properties containing prices to a single total

--- 05-iteration-array-methods/10-reduce/script.js
+++ 05-iteration-array-methods/10-reduce/script.js
@@ -23,3 +23,13 @@ const sum3 = () => {
 };
 
 console.log(sum3());
+
+const cart = [
+  { id: 1, name: "Product 1", price: 130 },
+  { id: 2, name: "Product 2", price: 150 },
+  { id: 3, name: "Product 3", price: 170 },
+];
+
+const total = cart.reduce((acc, product) => acc + product.price, 0);
+
+console.log(total);
