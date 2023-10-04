d11dd0c2450525c479ff9a6cc31797fada22c81a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 13:04:57 -0700

    Show how an if else statement can be turned into a ternary operator

---
+++ 04-logic-control-flow/08-ternary-operator/script.js
@@ -0,0 +1,11 @@
+const age = 23;
+
+// Using an If Statement
+if (age >= 18) {
+  console.log("You Can Vote!");
+} else {
+  console.log("You Can't Vote Yet...");
+}
+
+// Using a Ternary Operator
+age >= 18 ? console.log("You Can Vote!") : console.log("You Can't Vote Yet...");
