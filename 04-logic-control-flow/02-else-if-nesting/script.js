732092e6ac834401af11a13c9a14c35e42db8dbd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 12:19:18 -0700

    Show ways that if, else if, and else statements can be nested and have multiple conditions evaluated

--- 04-logic-control-flow/02-else-if-nesting/script.js
+++ 04-logic-control-flow/02-else-if-nesting/script.js
@@ -1,11 +1,39 @@
-const d = new Date(10, 30, 2022, 13, 0, 0);
+const d = new Date(10, 30, 2022, 20, 0, 0);
 const hour = d.getHours();
 
 // If, Else If, and Else
+// if (hour < 12) {
+//   console.log("Good morning!");
+// } else if (hour < 18) {
+//   console.log("Good afternoon!");
+// } else {
+//   console.log("Good night!");
+// }
+
+// Nested If
 if (hour < 12) {
   console.log("Good morning!");
+
+  if (hour === 6) {
+    console.log("Wake Up!");
+  }
 } else if (hour < 18) {
   console.log("Good afternoon!");
 } else {
   console.log("Good night!");
+
+  if (hour >= 20) {
+    console.log("Time for Bed!");
+  }
+}
+
+// Multiple Conditions in an If Statement
+// AND
+if (hour >= 7 && hour < 15) {
+  console.log("It is work time!");
+}
+
+// OR
+if (hour === 6 || hour === 20) {
+  console.log("Brush your teeth!");
 }
