d51b307e435438bd5291b3f2f03e77de2d393058
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:47:45 -0700

    Get a specific property that's nested inside of a set of objects

--- 02-arrays-and-objects/06-object-spread-methods/script.js
+++ 02-arrays-and-objects/06-object-spread-methods/script.js
@@ -8,4 +8,15 @@ todo.completed = false;
 
 x = todo;
 
+const person = {
+  address: {
+    coords: {
+      lat: 42.9384,
+      lng: -71.3232,
+    },
+  },
+};
+
+x = person.address.coords.lat;
+
 console.log(x);
