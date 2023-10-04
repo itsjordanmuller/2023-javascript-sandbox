9c9856a328cdb520185c940df0e68f7afdbfb477
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 13:01:09 -0700

    Show how the and operator can be used in logical assignment for compact statements

--- 04-logic-control-flow/07-logical-assignment/script.js
+++ 04-logic-control-flow/07-logical-assignment/script.js
@@ -7,10 +7,26 @@ if (!a) {
   a = 10;
 }
 
-// Compact Using OR
+// Compact Using ||
 a = a || 10;
 
 // Even Smaller Using ||=
 a ||= 10;
 
 console.log(a);
+
+// &&= Assigns the Right Side Value Only if the Left is a Truthy Value
+
+let b = 20;
+
+if (b) {
+  b = 30;
+}
+
+// Compact Using &&
+b = b && 30;
+
+// Even Smaller Using &&=
+b &&= 30;
+
+console.log(b);
