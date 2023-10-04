819f51f51cf54d118f8b44b6337c20e9fcfc2212
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:04:09 -0700

    Show various ways that a forEach loop can be used

---
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -0,0 +1,16 @@
+const socials = ["LinkedIn", "Facebook", "Instagram", "TikTok"];
+
+// console.log(socials.__proto__);
+
+// Use a forEach Loop
+// socials.forEach(function (social) {
+//   console.log(social);
+// });
+
+// Shorter Version With Arrow Function
+// socials.forEach((social) => {
+//   console.log(social);
+// });
+
+// Even Shorter Version with No Curly Braces
+socials.forEach((social) => console.log(social));
