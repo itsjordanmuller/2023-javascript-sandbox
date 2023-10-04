bf5085f2a6ebab8f7fa742baad14246d7193b150
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:38:59 -0700

    Show how while loops can be nested and where to set incrementing variables

--- 05-iteration-array-methods/03-while-do-while/script.js
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -1,5 +1,5 @@
 // While Loop
-let i = 0;
+// let i = 0;
 
 // while (i <= 20) {
 //   console.log(`Number ${i}`);
@@ -7,9 +7,24 @@ let i = 0;
 // }
 
 // Loop Over Arrays
-const arr = [10, 20, 30, 40];
+// const arr = [10, 20, 30, 40];
 
-while (i < arr.length) {
-  console.log(arr[i]);
+// while (i < arr.length) {
+//   console.log(arr[i]);
+//   i++;
+// }
+
+// Nesting While Loops
+let i = 1;
+
+while (i <= 5) {
+  console.log(`Number: ${i}`);
+
+  let j = 1;
+  while (j <= 5) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    j++;
+  }
+
   i++;
 }
