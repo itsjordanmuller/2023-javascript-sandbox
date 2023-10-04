0ea61a9caf5b305055701e3b94bb4060a5e7f775
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:16:19 -0700

    Concatenate the values of an array onto another array to include them as actual string values

--- 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
+++ 02-arrays-and-objects/03-array-nesting-concat-spread/script.js
@@ -3,8 +3,19 @@ let x;
 const fruits = ["Apple", "Orange", "Peach"];
 const berries = ["Strawberry", "Blueberry", "Raspberry"];
 
-fruits.push(berries);
+// Push an Array into Another Array to Nest It
+// fruits.push(berries);
 
-x = fruits[3][1];
+// Get a Value from a Nested Array By Going Another Level Down
+// x = fruits[3][1];
+
+// Create a New Array with Two Arrays
+const allFruits = [fruits, berries];
+
+// Get a Value from the Nested Arrays
+x = allFruits[1][2];
+
+// Concatenate the Values of an Array onto Another Array
+x = fruits.concat(berries);
 
 console.log(x);
