9e5061b01cc4a54d0607b5a90fdf77c4b2ae6649
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 13:11:23 -0700

    Use AND logical operator inside of a ternary to avoid needing an else action

--- 04-logic-control-flow/08-ternary-operator/script.js
+++ 04-logic-control-flow/08-ternary-operator/script.js
@@ -1,4 +1,4 @@
-const age = 23;
+const age = 17;
 
 // Using an If Statement
 if (age >= 18) {
@@ -9,3 +9,34 @@ if (age >= 18) {
 
 // Using a Ternary Operator
 age >= 18 ? console.log("You Can Vote!") : console.log("You Can't Vote Yet...");
+
+// Assigning a Conditional Value to a Variable
+const canVote1 = age >= 18 ? true : false;
+const canVote2 = age >= 18 ? "You Can Vote!" : "You Can't Vote Yet...";
+
+console.log(canVote1);
+console.log(canVote2);
+
+// Multiple Statements
+
+const auth = true;
+// let redirect;
+
+// if (auth) {
+//   alert("Welcome to the Dashboard!");
+//   redirect = "/dashboard";
+// } else {
+//   alert("Access Denied");
+//   redirect = "/login";
+// }
+
+// Does The Same As Above Code, Just Shorter
+const redirect = auth
+  ? (alert("Welcome to the Dashboard"), "/dashboard")
+  : (alert("Access Denied"), "/login");
+
+console.log(redirect);
+
+// auth ? console.log("Welcome to the Dashboard") : null;
+// Instead You Can Use:
+auth && console.log("Welcome to the Dashboard");
