dfbb260c193b6a0346493cc2fcd1319047eb2c07
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:29:00 -0700

    Show the same functionality as map, using forEach instead

--- 05-iteration-array-methods/09-map/script.js
+++ 05-iteration-array-methods/09-map/script.js
@@ -1,5 +1,15 @@
 const numbers = [1, 2, 3, 4, 5];
 
+// Loop Through Array with map
 const doubleNumbers = numbers.map((number) => number * 2);
 
 console.log(doubleNumbers);
+
+// Same with forEach
+const doubleNumbers2 = [];
+
+numbers.forEach((number) => {
+  doubleNumbers2.push(number * 2);
+});
+
+console.log(doubleNumbers2);
