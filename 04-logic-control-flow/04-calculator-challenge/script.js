46fb2dd4198791df0d517c57245a8d74571d3f8f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 12:30:46 -0700

    Create a calculator function that takes two numbers and an operator using a switch statement with an error message as default

---
+++ 04-logic-control-flow/04-calculator-challenge/script.js
@@ -0,0 +1,24 @@
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+      break;
+    case "-":
+      return num1 - num2;
+      break;
+    case "*":
+      return num1 * num2;
+      break;
+    case "/":
+      return num1 / num2;
+      break;
+    default:
+      return "Error, Operator Not Found\nPlease use +, -, *, or /";
+  }
+}
+
+console.log(calculator(5, 2, "+"));
+console.log(calculator(5, 2, "-"));
+console.log(calculator(5, 2, "*"));
+console.log(calculator(5, 2, "/"));
+console.log(calculator(5, 2, "&"));
