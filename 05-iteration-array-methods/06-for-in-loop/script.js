48b87178d96093dce6100147fb8d299ee71990d3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:57:03 -0700

    Get key sin color object uing a for in loop

---
+++ 05-iteration-array-methods/06-for-in-loop/script.js
@@ -0,0 +1,11 @@
+const colorObj = {
+  color1: "red",
+  color2: "orange",
+  color3: "yellow",
+  color4: "green",
+};
+
+// Get Keys in Color Object using For In Loop
+for (const key in colorObj) {
+  console.log(key);
+}
