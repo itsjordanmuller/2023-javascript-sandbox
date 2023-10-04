44cd854a239e289062f95feb8629fb32db25871d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:36:51 -0700

    Loop over an array using a while loop by using the index of the loop as the index for the array

--- 05-iteration-array-methods/03-while-do-while/script.js
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -1,7 +1,15 @@
 // While Loop
 let i = 0;
 
-while (i <= 20) {
-  console.log(`Number ${i}`);
+// while (i <= 20) {
+//   console.log(`Number ${i}`);
+//   i++;
+// }
+
+// Loop Over Arrays
+const arr = [10, 20, 30, 40];
+
+while (i < arr.length) {
+  console.log(arr[i]);
   i++;
 }
