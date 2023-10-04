476e0ff7567422bb70bb22355ac37464ac835834
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 14:53:51 -0700

    Access a specific property in an array by using a for of loop and the dot notation for selecting properties

--- 05-iteration-array-methods/05-for-of-loop/script.js
+++ 05-iteration-array-methods/05-for-of-loop/script.js
@@ -1,4 +1,5 @@
 const items = ["Book", "Table", "Chair", "Kite"];
+const users = [{ name: "Jordan" }, { name: "Kate" }, { name: "Steve" }];
 
 // Default Way to Loop Through Array with For Loop
 // for (let i = 0; i < items.length; i++) {
@@ -6,6 +7,11 @@ const items = ["Book", "Table", "Chair", "Kite"];
 // }
 
 // Does the Same Thing, But Shorter
-for (const item of items) {
-  console.log(item);
+// for (const item of items) {
+//   console.log(item);
+// }
+
+// Access a Specific Property in An Array
+for (const user of users) {
+  console.log(user.name);
 }
