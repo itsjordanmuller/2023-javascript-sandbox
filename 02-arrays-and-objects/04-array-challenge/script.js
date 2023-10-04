4731b6742f3cd03a50553083f3fc5da5693af499
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:31:03 -0700

    Complete challenge 2 by creating an array of two arrays, one of which is sliced to remove the extra 5, flatten this to an single level array

--- 02-arrays-and-objects/04-array-challenge/script.js
+++ 02-arrays-and-objects/04-array-challenge/script.js
@@ -7,3 +7,13 @@ arr.reverse();
 
 console.log(arr);
 // Expected Result: [6, 5, 4, 3, 2, 1, 0];
+
+// Challenge 2
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+arr3 = Array.of(arr1.slice(0, 4), arr2).flat();
+
+console.log(arr3);
+// Expected Result: [1,2,3,4,5,6,7,8,9,10]
