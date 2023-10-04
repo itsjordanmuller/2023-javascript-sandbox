180e4ec81dd285b357f0719e014da67f7aee8b52
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 18:01:58 -0700

    Create objects with variables, if they have the same name we don't have to set values manually

---
+++ 02-arrays-and-objects/07-destructuring-naming/script.js
@@ -0,0 +1,17 @@
+const firstName = "Jordan";
+const lastName = "Muller";
+const age = 30;
+
+const person = {
+  firstName: firstName,
+  lastName: lastName,
+  age: age,
+};
+
+const person2 = {
+  firstName,
+  lastName,
+  age,
+};
+
+console.log(person);
