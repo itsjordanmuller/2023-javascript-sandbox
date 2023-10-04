8adcb1b4720c9bc667672edbbf9d726460350820
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-03 17:01:22 -0700

    Unshift and then shift a value on and then off of the front of an array

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -1,9 +1,15 @@
 const arr = [50, 40, 30, 20, 10];
 
-// Push a Value Onto an Array
+// Push a Value Onto the End of an Array
 arr.push(0);
 
-// Pop a Value Off an Array
+// Pop a Value Off the End of an Array
 arr.pop(0);
+
+// Unshift a Value Onto the Front of an Array
+arr.unshift(60);
+
+// Shift a Value Off the Front of an Array
+arr.shift(60);
 
 console.log(arr);
