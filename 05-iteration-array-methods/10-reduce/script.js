20cbd228ce58dce0deddfedd7373fb1043ec15ce
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-04 15:45:10 -0700

    Use reduce to take the sum of an array by accumulating values until all values have been looped through

---
+++ 05-iteration-array-methods/10-reduce/script.js
@@ -0,0 +1,8 @@
+// Reduce an Array to a Single Value
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sum = numbers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum);
