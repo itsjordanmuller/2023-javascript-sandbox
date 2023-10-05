b605db2e66b309e928aa1fa7556050a938907b82
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 15:13:11 -0700

    Show how parent and sibling elements can be selected/targeted in many different ways

--- 06-document-object-model/05-traversing-the-dom-elements/script.js
+++ 06-document-object-model/05-traversing-the-dom-elements/script.js
@@ -16,4 +16,23 @@ parent.firstElementChild.innerText = "Child One";
 // Last Element Child
 parent.lastElementChild.innerText = "Child Three";
 
+// Get Parent Elements from a Child
+
+const child = document.querySelector(".child");
+
+output = child.parentElement;
+// Style the Parent by Selecting the Child and Traversing to Parent
+child.parentElement.style.border = "1px solid #ccc";
+child.parentElement.style.padding = "10px";
+
+// Sibling Elements
+const secondItem = document.querySelector(".child:nth-child(2)");
+output = secondItem;
+output = secondItem.nextElementSibling;
+
+// Select Next Sibling
+secondItem.nextElementSibling.style.color = "green";
+// Select Previous Sibling
+secondItem.previousElementSibling.style.color = "orange";
+
 console.log(output);
