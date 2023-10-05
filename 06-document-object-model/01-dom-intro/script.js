007e2e86424ae2d11ef04d9604657d5e4f3492cd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 14:02:23 -0700

    Show other ways that we can interact with and select items in the document

--- 06-document-object-model/01-dom-intro/script.js
+++ 06-document-object-model/01-dom-intro/script.js
@@ -9,3 +9,17 @@ console.log(document.body);
 console.log(document.body.innerHTML);
 console.log(document.body.innerText);
 console.log(document.links);
+
+// Replace Everything in the innerHTMl with The Following
+// document.body.innerHTML = "<h1>Hello World!</h1>";
+
+// Inserts as the next element in the Document
+// document.write("Hello from JS!");
+
+// Selecting Using a Specific ID
+console.log(document.getElementById("main"));
+const main = document.getElementById("main");
+// main.innerHTML = "<h1>Hello from main!</h1>";
+
+// Using a Query Selector
+document.querySelector("#main h1").innerText = "Hello DOM!";
