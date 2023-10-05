210b5cc392f9003eeadf84dc15839d86b339c39e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 13:56:11 -0700

    Show how document is part of the window, and how we can get it's properties/methods

---
+++ 06-document-object-model/01-dom-intro/script.js
@@ -0,0 +1,5 @@
+// We Can Log Document from the Window
+console.log(window.document);
+// Use dir to check Properties & Methods of the Document
+// No Need to Use window, since it's the top level object
+console.dir(document);
