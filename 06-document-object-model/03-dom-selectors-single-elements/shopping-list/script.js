8d9f0f68fe53237626b5925b42f153b16b7ebe94
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 14:32:25 -0700

    Show some examples of how query selectors can be used to get specific elements by various properties

--- 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
+++ 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
@@ -1,3 +1,4 @@
+// Get Element by ID
 // document.getElementById();
 
 // console.log(document.getElementById("app-title"));
@@ -24,3 +25,21 @@ title.style.color = "yellow";
 title.style.backgroundColor = "black";
 title.style.padding = "10px";
 title.style.borderRadius = "10px";
+
+// Query Selector
+// document.querySelector()
+
+// First Element
+console.log(document.querySelector("h1"));
+// First Element with ID app-title
+console.log(document.querySelector("#app-title"));
+// First Element with Class of container
+console.log(document.querySelector(".container"));
+// First Input Element with Text Set as Type Attribute
+console.log(document.querySelector("input[type='text']"));
+// 2nd List Item Element
+console.log(document.querySelector("li:nth-child(2)").innerText);
+
+const secondItem = document.querySelector("li:nth-child(2)");
+secondItem.innerText = "Apple Juice";
+secondItem.style.color = "red";
