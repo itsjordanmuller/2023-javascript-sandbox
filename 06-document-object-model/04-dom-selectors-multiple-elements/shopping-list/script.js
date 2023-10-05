e0652b2cccad3a1efbf5d1812dd6ca31f73d5bb1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 14:45:13 -0700

    Show other ways we can get multiple elements/items selected
    In most cases we will use querySelectorAll for this

--- 06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
+++ 06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
@@ -8,18 +8,36 @@ console.log(listItems[1]);
 console.log(listItems[1].innerText);
 
 // Use a forEach to Loop Through NodeList to Style Each Item
-listItems.forEach((item, index) => {
-  item.style.color = "red";
+// listItems.forEach((item, index) => {
+//   item.style.color = "red";
 
-  if (index === 1) {
-    item.remove();
-  }
-  if (index === 0) {
-    item.innerHTML = `
-        Pomegranates
-        <button class="remove-item btn-link text-red">
-        <i class="fa-solid fa-xmark"></i>
-        </button>
-    `;
-  }
+//   if (index === 1) {
+//     item.remove();
+//   }
+//   if (index === 0) {
+//     item.innerHTML = `
+//         Pomegranates
+//         <button class="remove-item btn-link text-red">
+//         <i class="fa-solid fa-xmark"></i>
+//         </button>
+//     `;
+//   }
+// });
+
+// Get Elements by Class Name
+// Returns an HTMLCollection instead of a NodeList
+// No Need for . for class
+const listItems2 = document.getElementsByClassName("item");
+console.log(listItems2);
+
+// Must Convert to An Array Before Using forEach
+const listItemsArray = Array.from(listItems2);
+
+listItemsArray.forEach((item) => {
+  console.log(item.innerText);
 });
+
+// Get Elements by Tag Name
+// Returns HTML Collection
+const listItems3 = document.getElementsByTagName("li");
+console.log(listItems3[0].innerText);
