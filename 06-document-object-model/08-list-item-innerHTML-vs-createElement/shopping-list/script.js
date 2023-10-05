8ea2263fb7e6b2431ba21de3c5a00a433fa24ad1
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 15:42:19 -0700

    Show a more performant and clean way of creating elements in the DOM

--- 06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/script.js
+++ 06-document-object-model/08-list-item-innerHTML-vs-createElement/shopping-list/script.js
@@ -13,3 +13,23 @@ ${item}
 createListItem("Eggs");
 
 // Performant & Clean
+function createNewItem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement("button");
+  button.className = "remove-item btn-link text-red";
+
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+
+  button.appendChild(icon);
+
+  li.appendChild(button);
+
+  console.log(li.innerHTML);
+
+  document.querySelector("ul").appendChild(li);
+}
+
+createNewItem("Cheese");
