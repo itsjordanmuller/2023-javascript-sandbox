266458f046a057aaa0c5e2b621fdc5bef14dd274
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 16:25:02 -0700

    Select relevant elements using query selector, add an onclick event to the button to log a value to console

---
+++ 06-document-object-model/14-styles-classes/script.js
@@ -0,0 +1,9 @@
+const text = document.querySelector("p");
+const itemList = document.querySelector(".item-list");
+const items = itemList.querySelectorAll("li");
+
+function run() {
+  console.log(1);
+}
+
+document.querySelector("button").onclick = run;
