83af3875dfb58a4e2728f397347ed9f5782fdef8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 13:58:35 -0700

    Show some different parts that can be targeted using the document

--- 06-document-object-model/01-dom-intro/script.js
+++ 06-document-object-model/01-dom-intro/script.js
@@ -3,3 +3,9 @@ console.log(window.document);
 // Use dir to check Properties & Methods of the Document
 // No Need to Use window, since it's the top level object
 console.dir(document);
+
+// Different Portions of the Document Can Be Targeted
+console.log(document.body);
+console.log(document.body.innerHTML);
+console.log(document.body.innerText);
+console.log(document.links);
