5f0568a9ef7cb3e6648468ffbed4d4cfe2544545
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 14:26:02 -0700

    Show some ways to get and change content and styles of elements by getting element by ID

--- 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
+++ 06-document-object-model/03-dom-selectors-single-elements/shopping-list/script.js
@@ -12,4 +12,15 @@ document.getElementById("app-title").setAttribute("class", "title");
 
 // Set a Variable to Avoid Repeating Code
 const title = document.getElementById("app-title");
+
+// Get/Change Content
 console.log(title);
+title.textContent = "Hello World!";
+title.innerText = "Hello Again!";
+title.innerHTML = "<strong>Shopping List</strong>";
+
+// Change Styles
+title.style.color = "yellow";
+title.style.backgroundColor = "black";
+title.style.padding = "10px";
+title.style.borderRadius = "10px";
