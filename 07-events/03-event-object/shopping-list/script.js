a36147bb035faff6fe509466643a752479b849cc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-05 20:33:46 -0700

    Test out using the event object and the various properties we can get about where, when the event occurred

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -1,3 +1,42 @@
+const logo = document.querySelector("img");
+
+function onClick(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  //   e.target.style.backgroundColor = "black";
+}
+
+function onDrag(e) {
+  document.querySelector("h1").textContent = `X ${e.clientX} Y ${e.clientY}`;
+}
+
+// Event Object Will Show in Console
+// logo.addEventListener("click", function (e) {
+//   console.log(e);
+// });
+logo.addEventListener("click", onClick);
+logo.addEventListener("drag", onDrag);
+
+document.body.addEventListener("click", function (e) {
+  //   console.log(e.target);
+  //   console.log(e.currentTarget);
+  console.log(e.type);
+  console.log(e.timeStamp);
+  console.log("clientX", e.clientX);
+  console.log("clientY", e.clientY);
+  console.log("offsetX", e.offsetX);
+  console.log("offsetY", e.offsetY);
+  console.log("pageX", e.pageX);
+  console.log("pageY", e.pageY);
+  console.log("screenX", e.screenX);
+  console.log("screenY", e.screenY);
+});
+
+document.querySelector("a").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("Link Broke!");
+});
+
 /*
 - `target` - The element that triggered the event
 - `currentTarget` - The element that the event listener is attached to (These are the same in this case
