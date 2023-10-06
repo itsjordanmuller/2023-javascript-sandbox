70ba3331bfd550b14a0025cb050bd508a6b08c29
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-06 16:06:02 -0700

    Add an event listener onto the text input form to check if input is empty on submit, prevent default behavior

--- 08-shopping-list-project/shopping-list/script.js
+++ 08-shopping-list-project/shopping-list/script.js
@@ -1,3 +1,17 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+  e.preventDefault();
+
+  // Validate Input
+  if (itemInput.value === "") {
+    alert("Item Input is Empty! Please add an item!");
+    return;
+  }
+  // Check to Ensure Filled Inputs are Detected
+  // console.log("Success!");
+}
+
+itemForm.addEventListener("submit", addItem);
