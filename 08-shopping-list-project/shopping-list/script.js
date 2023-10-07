c7dd6d6c4254d9040c974c45baa4e72f95123858
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-06 21:42:09 -0700

    Refactor the remove item function to make it invokable from new item click function

--- 08-shopping-list-project/shopping-list/script.js
+++ 08-shopping-list-project/shopping-list/script.js
@@ -86,13 +86,18 @@ function getItemsFromStorage() {
   return itemsFromStorage;
 }
 
-function removeItem(e) {
+function onClickItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    if (confirm("Are you sure you want to delete?")) {
-      e.target.parentElement.parentElement.remove();
+    removeItem(e.target.parentElement.parentElement);
+  }
+}
 
-      checkUI();
-    }
+function removeItem(item) {
+  if (confirm("Are you sure?")) {
+    // Remove Item from DOM
+    item.remove();
+
+    checkUI();
   }
 }
 
@@ -137,7 +142,7 @@ function checkUI() {
 // Initialize App
 function init() {
   itemForm.addEventListener("submit", onAddItemSubmit);
-  itemList.addEventListener("click", removeItem);
+  itemList.addEventListener("click", onClickItem);
   clearBtn.addEventListener("click", clearItems);
   itemFilter.addEventListener("input", filterItems);
   document.addEventListener("DOMContentLoaded", displayItems);
