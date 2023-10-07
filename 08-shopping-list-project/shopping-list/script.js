f01ba233c51186eb914ea1c3284ec2be4660ad95
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-06 21:10:29 -0700

    Log the list items that are currently in the item list as well as the filter text to facilitate filtering

--- 08-shopping-list-project/shopping-list/script.js
+++ 08-shopping-list-project/shopping-list/script.js
@@ -64,9 +64,17 @@ function clearItems() {
 }
 
 function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+
   const text = e.target.value.toLowerCase();
 
   console.log(text);
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    console.log(itemName);
+  });
 }
 
 function checkUI() {
