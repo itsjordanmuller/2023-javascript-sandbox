17ebe2c1d4988de81951aafc094ddf57faad0533
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 17:57:03 -0700

    Define a function to change h1 heading text content, use just the function name in a timeout

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -2,6 +2,11 @@
 setTimeout(function () {
   console.log("Hello from callback");
 }, 2000);
+setTimeout(changeText, 2000);
 
 // Runs Before Function with Timeout
 console.log("Hello from global scope");
+
+function changeText() {
+  document.querySelector("h1").textContent = "Hello from callback";
+}
