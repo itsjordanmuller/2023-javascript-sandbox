c33c70ca0f10bcb6c2d6e82dd6f82c9c3d31096c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:06:03 -0700

    Define a function that uses clearInterval to clear the constant setInterval with a button click

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -4,3 +4,10 @@ const intervalID = setInterval(myCallback, 1000);
 function myCallback() {
   console.log(Date.now());
 }
+
+// clearInterval to Stop the Current setInterval
+function stopChange() {
+  clearInterval(intervalID);
+}
+
+document.getElementById("stop").addEventListener("click", stopChange);
