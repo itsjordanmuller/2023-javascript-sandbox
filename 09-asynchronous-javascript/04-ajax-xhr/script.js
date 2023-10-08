e750d28bb955e2d480b8ebf0abcc824500e50d1b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:39:44 -0700

    Take some notes on the 5 possible states readyState can have

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -4,5 +4,12 @@ const xhr = new XMLHttpRequest();
 // Initializing the request. The 'GET' method is used to retrieve data from the JSON file
 xhr.open("GET", "./movies.json");
 
+// readyState has 5 Possible States
+// - 0: request not initialized
+// - 1: server connection established
+// - 2: request received
+// - 3: processing request
+// - 4: request finished and response is ready
+
 // Function will be called whenever the state of the request changes.
 xhr.onreadystatechange = function () {};
