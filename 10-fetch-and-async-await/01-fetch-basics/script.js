81603468b7db0d0d6f6941102e32b2f2a707d341
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-08 16:49:58 -0700

    Fetch movies from a JSON file, return the response object using the json method, and then log that data to the console

---
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -0,0 +1,14 @@
+// Fetch from Window
+// window.fetch();
+// Fetch without window Object and with a then
+fetch("./movies.json")
+  .then((response) => {
+    // console.log(response);
+
+    // Return Response to Next then
+    return response.json();
+  })
+  .then((data) => {
+    // Take the response data and log to console
+    console.log(data);
+  });
