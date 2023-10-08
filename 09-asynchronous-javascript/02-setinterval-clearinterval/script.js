e5a675e786ff7362591a177e48282d7c3acf5231
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:13:33 -0700

    Define a function to change the color of the body background from white to black, with the text opposite, set start button to call function

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -1,8 +1,26 @@
 // setInterval to Call Function at Set Time Between Repetitions
-const intervalID = setInterval(myCallback, 1000);
+// const intervalID = setInterval(myCallback, 1000);
 
-function myCallback() {
-  console.log(Date.now());
+// function myCallback() {
+//   console.log(Date.now());
+// }
+
+let intervalID;
+
+function startChange() {
+  if (!intervalID) {
+    intervalID = setInterval(changeColor, 1000);
+  }
+}
+
+function changeColor() {
+  if (document.body.style.backgroundColor === "white") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
 }
 
 // clearInterval to Stop the Current setInterval
@@ -10,4 +28,5 @@ function stopChange() {
   clearInterval(intervalID);
 }
 
+document.getElementById("start").addEventListener("click", startChange);
 document.getElementById("stop").addEventListener("click", stopChange);
