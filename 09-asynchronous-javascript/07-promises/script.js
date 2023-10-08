456674b570c8a3e6ac66da9cf2e47c500cff8b75
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 19:14:41 -0700

    Create another promise and asnychronous task, to show order of actions

--- 09-asynchronous-javascript/07-promises/script.js
+++ 09-asynchronous-javascript/07-promises/script.js
@@ -2,15 +2,22 @@
 const promise = new Promise((resolve, reject) => {
   // Do Some Asynchronous Task
   setTimeout(() => {
-    console.log("Async task complete");
+    console.log("Async task 1 complete");
     resolve();
   }, 1000);
 });
 
 // After Promise, Run This Code
 promise.then(() => {
-  console.log("Promise consumed...");
+  console.log("Promise 1 consumed...");
 });
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async task 2 complete");
+    resolve();
+  }, 1000);
+}).then(() => console.log("Promise 2 consumed..."));
 
 // Runs Before Rest, Due to Global Scope
 console.log("Hello from global scope!");
