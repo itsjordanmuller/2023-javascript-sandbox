a396da8f9608ad75760d2438e54006829c9784bc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-08 16:51:39 -0700

    Show a shorter way of writing a fetch to a local JSON file

--- 10-fetch-and-async-await/01-fetch-basics/script.js
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -1,14 +1,21 @@
 // Fetch from Window
 // window.fetch();
 // Fetch without window Object and with a then
-fetch("./movies.json")
-  .then((response) => {
-    // console.log(response);
+// fetch("./movies.json")
+//   .then((response) => {
+//     // console.log(response);
 
-    // Return Response to Next then
-    return response.json();
-  })
+//     // Return Response to Next then
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // Take the response data and log to console
+//     console.log(data);
+//   });
+
+// Shorter Way of Writing
+fetch("./movies.json")
+  .then((response) => response.json())
   .then((data) => {
-    // Take the response data and log to console
     console.log(data);
   });
