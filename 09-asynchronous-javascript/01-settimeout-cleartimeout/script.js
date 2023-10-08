ad74fe7749a59bdec3d291abd3142a77800368be
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:00:45 -0700

    Create a constant with a timeout to call a function, and make a button clear that timeout when clicked

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -1,12 +1,22 @@
 // Set a Timeout on a Function
-setTimeout(function () {
-  console.log("Hello from callback");
-}, 2000);
-setTimeout(changeText, 2000);
+// setTimeout(function () {
+//   console.log("Hello from callback");
+// }, 2000);
+// setTimeout(changeText, 2000);
 
 // Runs Before Function with Timeout
-console.log("Hello from global scope");
+// console.log("Hello from global scope");
 
 function changeText() {
   document.querySelector("h1").textContent = "Hello from callback";
 }
+
+// Creating a Constant with a setTimeout & Function
+const timerId = setTimeout(changeText, 3000);
+
+// ClearTimeout Using Button to Clear the timerId
+document.querySelector("#cancel").addEventListener("click", () => {
+  console.log(timerId);
+  clearTimeout(timerId);
+  console.log("Timer Cancelled!");
+});
