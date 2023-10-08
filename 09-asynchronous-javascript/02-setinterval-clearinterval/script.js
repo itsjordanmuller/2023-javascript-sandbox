6af436c712a7544d5e0d7bd23ab1fab168f8ab9e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:17:25 -0700

    Define a new function to set a random background color, use it as the function invoked by the start color change button

--- 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -9,18 +9,23 @@ let intervalID;
 
 function startChange() {
   if (!intervalID) {
-    intervalID = setInterval(changeColor, 1000);
+    intervalID = setInterval(changeRandomColor, 1000);
   }
 }
 
-function changeColor() {
-  if (document.body.style.backgroundColor === "white") {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-  } else {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-  }
+// function changeColor() {
+//   if (document.body.style.backgroundColor === "white") {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = "white";
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//   }
+// }
+
+function changeRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = `#${randomColor}`;
 }
 
 // clearInterval to Stop the Current setInterval
