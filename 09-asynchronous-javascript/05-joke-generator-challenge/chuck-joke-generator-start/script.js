df40e95b736e40bff20d514b5b390d7a8dfa6c5e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-07 18:58:52 -0700

    Get data from Chuck Norris Joke API and set the text to that value, set button to get a new joke and a joke to be requested initially

---
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator-start/script.js
@@ -0,0 +1,22 @@
+const jokeBtn = document.getElementById("joke-btn");
+const jokeText = document.getElementById("joke");
+
+function getJoke() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      // console.log(JSON.parse(this.responseText));
+      const data = JSON.parse(this.responseText);
+      jokeText.innerText = data.value;
+    }
+  };
+
+  xhr.send();
+}
+
+jokeBtn.addEventListener("click", getJoke);
+
+getJoke();
