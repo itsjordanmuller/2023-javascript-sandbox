662d2f1632310c8fe0ae2c88fd766b61c08cb204
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-08 17:36:01 -0700

    Refactor to avoid repeating code to use values in template literal strings

--- 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -4,27 +4,32 @@ const emailEl = document.getElementById("email");
 const phoneEl = document.getElementById("phone");
 const locationEl = document.getElementById("location");
 const ageEl = document.getElementById("age");
+const bodyEl = document.body;
 
 function fetchUser() {
   fetch("https://randomuser.me/api/")
     .then((res) => res.json())
     .then((data) => {
+      const { name, email, phone, location, dob, gender } = data.results[0];
+      const { first, last } = name;
+      const { city, state } = location;
+      const { age } = dob;
+
       nameEl.innerHTML = `
-        <span class="font-bold">Name: </span>${data.results[0]["name"]["first"]} ${data.results[0]["name"]["last"]}
-    `;
+        <span class="font-bold">Name: </span>${first} ${last}
+      `;
       emailEl.innerHTML = `
-        <span class="font-bold">Email: </span>
-                ${data.results[0]["email"]}
-        `;
+        <span class="font-bold">Email: </span>${email}
+      `;
       phoneEl.innerHTML = `
-        <span class="font-bold">Phone: </span> ${data.results[0]["phone"]}
-    `;
+        <span class="font-bold">Phone: </span>${phone}
+      `;
       locationEl.innerHTML = `
-      <span class="font-bold">Location: </span> ${data.results[0]["location"]["city"]} ${data.results[0]["location"]["state"]}
-    `;
+        <span class="font-bold">Location: </span>${city}, ${state}
+      `;
       ageEl.innerHTML = `
-      <span class="font-bold">Age: </span> ${data.results[0]["dob"]["age"]}
-    `;
+        <span class="font-bold">Age: </span>${age}
+      `;
     });
 }
 
