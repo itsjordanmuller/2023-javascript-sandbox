198f635af97b36ef88876aa6548d5d330f49b29f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-08 20:16:33 -0700

    Use JSON placeholder API to simulate creating a post and sending a plaintext token to authenticate

---
+++ 10-fetch-and-async-await/03-fetch-options/script.js
@@ -0,0 +1,19 @@
+// POST Request to Create a Post
+function createPost({ title, body }) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      hello: "World",
+      token: "abc123",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+createPost({ title: "My Post", body: "This is my post!" });
