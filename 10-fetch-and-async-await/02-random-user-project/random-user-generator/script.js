2f8375d13f20cee844f271f2a9d13a8a89470f9d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-08 17:29:50 -0700

    Set the innerHTML of all elements based on the data provided by the random user API

--- 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
+++ 10-fetch-and-async-await/02-random-user-project/random-user-generator/script.js
@@ -1,17 +1,30 @@
+const generateBtn = document.getElementById("generate");
+const nameEl = document.getElementById("name");
+const emailEl = document.getElementById("email");
+const phoneEl = document.getElementById("phone");
+const locationEl = document.getElementById("location");
+const ageEl = document.getElementById("age");
+
 function fetchUser() {
   fetch("https://randomuser.me/api/")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      const gender = data.results[0]["gender"];
-      const firstName = data.results[0]["name"]["first"];
-      const lastName = data.results[0]["name"]["last"];
-      const email = data.results[0]["email"];
-      const phone = data.results[0]["phone"];
-      const city = data.results[0]["location"]["city"];
-      const state = data.results[0]["location"]["state"];
-      const age = data.results[0]["dob"]["age"];
-      console.log(gender, firstName, lastName, email, phone, city, state, age);
+      nameEl.innerHTML = `
+        <span class="font-bold">Name: </span>${data.results[0]["name"]["first"]} ${data.results[0]["name"]["last"]}
+    `;
+      emailEl.innerHTML = `
+        <span class="font-bold">Email: </span>
+                ${data.results[0]["email"]}
+        `;
+      phoneEl.innerHTML = `
+        <span class="font-bold">Phone: </span> ${data.results[0]["phone"]}
+    `;
+      locationEl.innerHTML = `
+      <span class="font-bold">Location: </span> ${data.results[0]["location"]["city"]} ${data.results[0]["location"]["state"]}
+    `;
+      ageEl.innerHTML = `
+      <span class="font-bold">Age: </span> ${data.results[0]["dob"]["age"]}
+    `;
     });
 }
 
