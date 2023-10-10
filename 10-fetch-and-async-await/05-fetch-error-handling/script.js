80b07005261a34eaf3d30cb475c893604a8ada34
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:06:02 -0700

    Log a few different properties of the response to get status, if status is ok and status text

--- 10-fetch-and-async-await/05-fetch-error-handling/script.js
+++ 10-fetch-and-async-await/05-fetch-error-handling/script.js
@@ -1,3 +1,4 @@
+// 200 - OK
 // fetch("https://httpstat.us/200")
 //   .then((response) => {
 //     return response;
@@ -6,27 +7,35 @@
 //     console.log("Success");
 //   });
 
-// fetch("https://httpstat.us/404")
+// 404 - Not Found
+fetch("https://httpstat.us/404")
+  .then((response) => {
+    // Check Status of Response (404)
+    console.log(response.status);
+    // Check if Response is OK (false)
+    console.log(response.ok);
+    // Check Text of Status ("Not Found")
+    console.log(response.statusText);
+    return response;
+  })
+  .then(() => {
+    console.log("Success");
+  })
+  // catch Doesn't Run
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Simulating a Network Error
+// TypeError Failed to Fetch
+// fetch("https:/hello123.net")
 //   .then((response) => {
 //     return response;
 //   })
 //   .then(() => {
 //     console.log("Success");
 //   })
-//   // catch Doesn't Run
+//   // catch Does Work with Network Error
 //   .catch((error) => {
 //     console.log(error);
 //   });
-
-// Simulating a Network Error
-fetch("https:/hello123.net")
-  .then((response) => {
-    return response;
-  })
-  .then(() => {
-    console.log("Success");
-  })
-  // catch Does Work with Network Error
-  .catch((error) => {
-    console.log(error);
-  });
