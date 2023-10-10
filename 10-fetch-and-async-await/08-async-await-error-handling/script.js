a4e1720651166e878c53eb73537362878539e23f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:45:11 -0700

    Switch fetch request to 404 placeholder status code API, if response is not ok, throw error

--- 10-fetch-and-async-await/08-async-await-error-handling/script.js
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -1,6 +1,12 @@
 const getUsers = async () => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await fetch("https://httpstat.us/404");
+
+    if (!response.ok) {
+      throw new Error("Request Failed");
+    }
+
     const data = await response.json();
 
     console.log(data);
