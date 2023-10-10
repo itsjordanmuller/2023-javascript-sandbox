2051be37828205efe54a6c08a2b2a6dd8efb2643
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:01:53 -0700

    Simulate a 404 error to demonstrate how the catch doesn't run

--- 10-fetch-and-async-await/05-fetch-error-handling/script.js
+++ 10-fetch-and-async-await/05-fetch-error-handling/script.js
@@ -1,7 +1,19 @@
-fetch("https://httpstat.us/200")
+// fetch("https://httpstat.us/200")
+//   .then((response) => {
+//     return response;
+//   })
+//   .then(() => {
+//     console.log("Success");
+//   });
+
+fetch("https://httpstat.us/404")
   .then((response) => {
     return response;
   })
   .then(() => {
     console.log("Success");
+  })
+  // catch Doesn't Run
+  .catch((error) => {
+    console.log(error);
   });
