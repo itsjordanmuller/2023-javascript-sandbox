c7d5632ffd1cc8d5ea7cc743c972e106a081bea9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:32:37 -0700

    Show the syntax for using async await within an arrow function

--- 10-fetch-and-async-await/06-async-await/script.js
+++ 10-fetch-and-async-await/06-async-await/script.js
@@ -23,13 +23,23 @@ async function getUsers() {
   console.log(data);
 }
 
-getUsers();
+// getUsers();
 
 // Same Function Using then instead of async and await
-function getUsers2() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}
+// function getUsers2() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }
 
-getUsers2();
+// getUsers2();
+
+// Arrow Function Async Goes Before ()
+const getPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  console.log(data);
+};
+
+getPosts();
