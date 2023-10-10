d4e7b973fb64d3208c1ec5cbddaa631d6ccea114
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:11:45 -0700

    Check if response status is ok to throw a new error to the catch, then check for specific status codes and throw multiple specific errors

--- 10-fetch-and-async-await/05-fetch-error-handling/script.js
+++ 10-fetch-and-async-await/05-fetch-error-handling/script.js
@@ -8,14 +8,25 @@
 //   });
 
 // 404 - Not Found
-fetch("https://httpstat.us/404")
+fetch("https://httpstat.us/500")
   .then((response) => {
-    // Check Status of Response (404)
-    console.log(response.status);
-    // Check if Response is OK (false)
-    console.log(response.ok);
-    // Check Text of Status ("Not Found")
-    console.log(response.statusText);
+    // // Check Status of Response (404)
+    // console.log(response.status);
+    // // Check if Response is OK (false)
+    // console.log(response.ok);
+    // // Check Text of Status ("Not Found")
+    // console.log(response.statusText);
+    if (response.status === 404) {
+      throw new Error("Not Found Error");
+    } else if (response.status === 500) {
+      throw new Error("Server Error");
+    } else if (response.status !== 200) {
+      throw new Error("Request Failed");
+    }
+    // Test with response.ok
+    // if (!response.ok) {
+    //   throw new Error("Request Failed");
+    // }
     return response;
   })
   .then(() => {
