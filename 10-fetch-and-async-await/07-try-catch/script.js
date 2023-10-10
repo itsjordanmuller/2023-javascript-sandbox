34aee0a953aec4ccaaf9e30535b7daab05bdd638
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-10 14:39:47 -0700

    Use try and catch to try to call a function and catch any errors it may have

--- 10-fetch-and-async-await/07-try-catch/script.js
+++ 10-fetch-and-async-await/07-try-catch/script.js
@@ -1,9 +1,25 @@
 // With Async Await, Try Catch is the Preferred Method of Dealing with Errors
 
-// try to log x which isn't defined
+// // try to log x which isn't defined
+// try {
+//   console.log(x);
+//   // catch is triggered in event of error
+// } catch (error) {
+//   console.log(`Error: ${error}`);
+// }
+
+function double(number) {
+  if (isNaN(number)) {
+    throw new Error(`${number} is not a number`);
+  }
+  return number * 2;
+}
+
 try {
-  console.log(x);
-  // catch is triggered in event of error
+  //   const a = double(2);
+  //   throws error for NaN
+  const b = double("hello");
+  console.log(b);
 } catch (error) {
-  console.log(`Error: ${error}`);
+  console.log(`${error}`);
 }
