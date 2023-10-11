d58bb24e5ed97a29fc93ef986614c53a9eeeea27
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 16:41:14 -0700

    Use swiperjs library to create a modern mobile touch slider with movie posters and ratings of now playing movies

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -5,7 +5,7 @@ const global = {
 // Display 20 Most Popular Movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
-  console.log(results);
+  // console.log(results);
   results.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("card");
@@ -187,6 +187,59 @@ async function displayShowDetails() {
   document.getElementById("show-details").appendChild(showDetailsEl);
 }
 
+// Display Slider
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}"/>`
+          : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />
+          `
+      }
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+          1
+        )} / 10
+      </h4>
+    `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+  });
+
+  initSwiper();
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Display Backdrop on Details Pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement("div");
@@ -257,6 +310,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displaySlider();
       displayPopularMovies();
       //console.log("Home");
       break;
