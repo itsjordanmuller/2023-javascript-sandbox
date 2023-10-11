005836bf1d0be2d075f88601fe7ce63efacd7c58
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 16:02:57 -0700

    Go through each piece of the placeholder movie details data to replace it with real API data for the selected movie
    Define and use a function to add commas to the revenue and budget numbers

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -61,7 +61,65 @@ async function displayPopularShows() {
 // Display Specific Movie Details
 async function displayMovieDetails() {
   const movieID = window.location.search.split("=")[1];
-  console.log(movieID);
+  // console.log(movieID);
+  const movie = await fetchAPIData(`movie/${movieID}`);
+
+  const movieDetailsEl = document.createElement("div");
+  movieDetailsEl.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}"/>`
+              : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />
+              `
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map(
+              (company) => `
+            <span>${company.name}</span>
+          `
+            )
+            .join(", ")}</div>
+        </div>
+  `;
+
+  document.getElementById("movie-details").appendChild(movieDetailsEl);
 }
 
 // Fetch Data from TMDB API
@@ -100,6 +158,11 @@ function highlightActiveLink() {
       link.classList.add("active");
     }
   });
+}
+
+// Add Commas to Numbers
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // Initialize App
