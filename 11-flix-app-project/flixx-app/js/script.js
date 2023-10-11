8b64116e180f207deb19c4eb190623262afc6235
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 15:39:03 -0700

    Copy the display popular movies function and refactor it to make it work for shows, edit shows HTML to get rid of placeholder shows

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 Most Popular Movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
   console.log(results);
@@ -26,6 +27,34 @@ async function displayPopularMovies() {
         `;
 
     document.getElementById("popular-movies").appendChild(movieEl);
+  });
+}
+
+// Show 20 Most Popular Shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+  console.log(results);
+  results.forEach((show) => {
+    const showEl = document.createElement("div");
+    showEl.classList.add("card");
+    showEl.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}"/>`
+            : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />
+            `
+        }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+        `;
+
+    document.getElementById("popular-shows").appendChild(showEl);
   });
 }
 
@@ -79,7 +108,8 @@ function init() {
       console.log("Search");
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayPopularShows();
+      // console.log("Shows");
       break;
     case "/tv-details.html":
       console.log("TV Details");
