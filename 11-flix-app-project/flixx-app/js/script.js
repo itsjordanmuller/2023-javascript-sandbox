239bdff1acb35264648e1a432fc66f1237648cf2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-11 13:35:07 -0700

    Initialize the application with a simple router to log which page the user is on, call this function when DOM Content is loaded

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -3,3 +3,27 @@ const global = {
 };
 
 console.log(global.currentPage);
+
+// Initialize App
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case "/index.html":
+      console.log("Home");
+      break;
+    case "/search.html":
+      console.log("Search");
+      break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", init);
