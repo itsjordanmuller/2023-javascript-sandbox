e6a26e8d0977f4e0446668ac05e5ec070a67a151
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 16:44:10 -0700

    When the play button is clicked, play the song and replace with pause icon, pause song if already playing and clicked again

---
+++ 12-web-browser-apis/08-music-player-project/music-player/script.js
@@ -0,0 +1,49 @@
+const audio = document.querySelector("audio");
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const playBtnIcon = document.querySelector(".fa-play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Array of Song Titles
+const songs = ["hey", "summer", "ukulele"];
+
+// Keep Track of Songs
+let songIndex = 2;
+
+// Initially Load Song Details
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtnIcon.classList.remove("fa-play");
+  playBtnIcon.classList.add("fa-pause");
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtnIcon.classList.remove("fa-pause");
+  playBtnIcon.classList.add("fa-play");
+  audio.pause();
+}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
