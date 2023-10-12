1dd7e26b4e3f28f2a3738066c255d69222441c1e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 12:35:25 -0700

    Log the current lat, long, and accuacy data to console using GeoLocation API

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,4 +1,20 @@
-// getCurrentPosition()
+// Get Current Position
+function curSuccess(position) {
+  const coords = position.coords;
+  //   console.log(position);
+  console.log(`Latitude: ${coords.latitude}`);
+  console.log(`Longitude: ${coords.longitude}`);
+  console.log(`Accuracy: within ${coords.accuracy} meters`);
+}
 
+function curError(error) {
+  console.log(`Error: ${error.code} - ${error.message}`);
+}
 
-// watchPosition()
+const curOptions = {
+  enableHighAccuracy: true, // Use GPS if Available
+  timeout: 5000, // Time to Wait ot Stop Trying for Data
+  maximumAge: 0, // Do Not use a Cached Position
+};
+
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
