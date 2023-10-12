a9db49abe5eb26030e2fae4eedfdf321f07d0f89
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 12:40:42 -0700

    Define a function that watches the coordinates to see if the user has arrived at the target destination

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -18,3 +18,37 @@ const curOptions = {
 };
 
 navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
+
+const target = {
+  latitude: 37.8023727,
+  longitude: -122.4058273,
+};
+
+// Watch Position
+function watchSuccess(position) {
+  const coords = position.coords;
+
+  if (
+    target.latitude === coords.latitude &&
+    target.longitude === coords.longitude
+  ) {
+    console.log("You have reached your destination!");
+    navigator.geolocation.clearWatch(id);
+  }
+}
+
+function watchError(error) {
+  console.log(`Error: ${error.code} - ${error.message}`);
+}
+
+const watchOptions = {
+  enableHighAccuracy: true, // Use GPS if Available
+  timeout: 5000, // Time to Wait ot Stop Trying for Data
+  maximumAge: 0, // Do Not use a Cached Position
+};
+
+const id = navigator.geolocation.watchPosition(
+  watchSuccess,
+  watchError,
+  watchOptions
+);
