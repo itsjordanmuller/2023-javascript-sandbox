b4a0612f42daa06d7b6799009ff4c3aaf6341ff0
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 15:38:38 -0700

    Explore object created via constructor to see prototype object and the various methods it contains

--- 13-oop-constructors-prototypes/05-prototypes/script.js
+++ 13-oop-constructors-prototypes/05-prototypes/script.js
@@ -7,4 +7,14 @@ function Rectangle(name, width, height) {
   };
 }
 
-const rect = new Rectangle('Rect', 10, 10);
+const rect = new Rectangle("Rect", 10, 10);
+
+// When We Log Object, We Get the Properties, Methods, & Prototype Object
+console.log(rect);
+
+// 1. Looks in Properties & Methods
+// 2. Fails to Find toString
+// 3. Moves to Prototypes Object
+// 4. Continue Failing to Find Until it Gets to the Nested Prototype Object with the toString Method
+// 5. Call the toString method that was just found
+console.log(rect.toString());
