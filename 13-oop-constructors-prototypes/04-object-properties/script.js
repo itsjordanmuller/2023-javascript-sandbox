b965806147e0c8d24962d8de5f97efe78f4bfb68
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 15:19:07 -0700

    Delete the color property as well as the newly-created perimeter method from the second rectangle object

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -14,12 +14,27 @@ rect2 = new Rectangle("Rectangle 2", 10, 10);
 console.log(rect1.name, rect2["name"]);
 
 // Create New Property on Object & Set Value
+console.log("--- CREATE PROPERTY & SET VALUE ---");
 console.log("Before New Property", rect1);
 rect1.color = "red";
+rect2.color = "blue";
 console.warn("After New Property", rect1);
 
 // Create New Method on Object
+console.log("--- CREATE & CALL METHOD ---");
 console.log("Before New Method", rect1);
 rect2.perimeter = () => 2 * (rect2.width + rect2.height);
 console.warn("After New Method", rect1);
 console.log("Use New Method:", rect2.perimeter());
+
+// Delete a Property from an Object
+console.log("--- DELETE A PROPERTY ---");
+console.log("Before Deleting Property", rect2);
+delete rect2.color;
+console.warn("After Deleting Property", rect2);
+
+// Delete a Method from an Object
+console.log("--- DELETE A PROPERTY ---");
+console.log("Before Deleting Method", rect2);
+delete rect2.perimeter;
+console.warn("After Deleting Method", rect2);
