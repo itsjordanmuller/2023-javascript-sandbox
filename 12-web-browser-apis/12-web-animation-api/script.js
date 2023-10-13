f25a473f2292f3677327d29862f84237739cd562
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-12 21:36:42 -0700

    Animate the ball using JavaScript instead of CSS, that way we can dynamically control the values with input controls

---
+++ 12-web-browser-apis/12-web-animation-api/script.js
@@ -0,0 +1,28 @@
+const ball = document.getElementById("ball");
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const reverse = document.getElementById("reverse");
+const speedUp = document.getElementById("speed-up");
+const speedDown = document.getElementById("speed-down");
+
+const rollAnimation = [
+  {
+    transform: "rotate(0) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
+  {
+    color: "blue",
+    offset: 0.3,
+  },
+  {
+    transform: "rotate(360deg) translate3D(-50%, -50%, 0)",
+    color: "white",
+  },
+];
+
+const rollOptions = {
+  duration: 3000,
+  iteration: Infinity,
+};
+
+const roll = ball.animate(rollAnimation, rollOptions);
