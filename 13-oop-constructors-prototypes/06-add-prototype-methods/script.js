69067dfab29f491581a1f33755ee80f838a199c6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 15:43:35 -0700

    Show how a method can be moved from the constructor function to the prototype of that constructor and still be useable

--- 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
+++ 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
@@ -2,12 +2,15 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
 }
 
+// Relocate Function from Constructor to Prototype by Using Dot Notation and Removing this
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
 
-const rect = new Rectangle('Rect', 10, 10);
+const rect = new Rectangle("Rect", 10, 10);
 
 console.log(rect);
+// Still Able to Call Method from Prototype Object
+console.log(rect.area());
