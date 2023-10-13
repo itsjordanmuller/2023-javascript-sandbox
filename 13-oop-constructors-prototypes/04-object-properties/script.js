b6ce3ad23d1252e36b0de408e2a1d0ce0845a59c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 15:14:21 -0700

    Create a new method on a specific instance of an object, in this instance a perimeter calculation for the second rectangle

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -17,3 +17,9 @@ console.log(rect1.name, rect2["name"]);
 console.log("Before New Property", rect1);
 rect1.color = "red";
 console.warn("After New Property", rect1);
+
+// Create New Method on Object
+console.log("Before New Method", rect1);
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+console.warn("After New Method", rect1);
+console.log("Use New Method:", rect2.perimeter());
