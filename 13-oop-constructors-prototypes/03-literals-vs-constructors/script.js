ff87c8fe99b9881e7dfafadd5de6e24f6e42fd09
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 14:58:45 -0700

    Log the constructors and check if the string literal and string object are instances of the String constructor

--- 13-oop-constructors-prototypes/03-literals-vs-constructors/script.js
+++ 13-oop-constructors-prototypes/03-literals-vs-constructors/script.js
@@ -10,3 +10,12 @@ console.log(strLit[0]);
 
 // Unboxing - Turns Object Back into Literal
 console.log(strObj.valueOf(), typeof strObj.valueOf());
+
+// Constructors for Literal and Object Strings
+console.log(strLit.constructor);
+console.log(strObj.constructor);
+
+// False - Not an Instance of String Constructor
+console.log(strLit instanceof String);
+// True - String Constructor Used to Create
+console.log(strObj instanceof String);
