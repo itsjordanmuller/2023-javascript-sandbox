0a8f3430b9c91cc67e22b4c7687470238aff8ea3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 15:11:28 -0700

    Create two objects using constructor function, get names of each in different ways using dot and bracket notation

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,9 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+rect1 = new Rectangle("Rectangle 1", 20, 20);
+rect2 = new Rectangle("Rectangle 2", 10, 10);
+
+// Getting Values of Object Properties
+console.log(rect1.name, rect2["name"]);
