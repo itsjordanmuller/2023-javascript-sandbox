173ef89d04f3a9693d87baf51cb4ff41bc260b2b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 19:39:43 -0700

    Show the process of sealing properties of an object

--- 14-oop-classes-private-properties/09-freezing-sealing-properties/script.js
+++ 14-oop-classes-private-properties/09-freezing-sealing-properties/script.js
@@ -1,3 +1,26 @@
 // Sealing - Prevents properties from being added or removed. Can still be changed.
 
 // Freezing - Prevents properties from being added, removed or changed
+
+const rectObj = {
+  name: "Rect1",
+  width: 10,
+  height: 10,
+};
+
+// Get Value, Writeable, Enumerable, and Configurable Settings for All Properties
+let descriptors = Object.getOwnPropertyDescriptors(rectObj);
+console.log("Before Seal: ", descriptors);
+
+// Seal Configurable to False on All Properties
+Object.seal(rectObj);
+
+descriptors = Object.getOwnPropertyDescriptors(rectObj);
+console.log("After Seal: ", descriptors);
+
+rectObj.color = "red";
+delete rectObj.name;
+
+rectObj.width = 20;
+
+console.log(rectObj);
