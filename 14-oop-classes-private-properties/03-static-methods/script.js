f12e507b6f63dc449cadd0a86f82540ebd9b9d50
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 18:28:10 -0700

    Add a static method to a class and show how it can only be called from the class itself, not objects created by the class constructor

--- 14-oop-classes-private-properties/03-static-methods/script.js
+++ 14-oop-classes-private-properties/03-static-methods/script.js
@@ -4,4 +4,20 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  static getClass() {
+    return "Rectangle";
+  }
 }
+
+const rect1 = new Rectangle("Rect1", 10, 10);
+console.log(rect1.area());
+
+// This Won't Work, Since the Method is Static
+// console.log(rect1.getClass());
+// Call Method from the Class Instead
+console.log(Rectangle.getClass());
