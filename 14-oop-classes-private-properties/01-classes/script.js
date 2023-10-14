15a40ff1304457fce099a7dff95063ab7448887c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 18:10:24 -0700

    Create a class for constructor function, set constructor and properties

--- 14-oop-classes-private-properties/01-classes/script.js
+++ 14-oop-classes-private-properties/01-classes/script.js
@@ -1,20 +1,36 @@
-function Rectangle(height, width) {
-  this.height = height;
-  this.width = width;
+// Constructor Function with Prototype Methods
+
+// function Rectangle(height, width) {
+//   this.height = height;
+//   this.width = width;
+// }
+
+// Rectangle.prototype.area = function () {
+//   return this.height * this.width;
+// };
+
+// Rectangle.prototype.perimeter = function () {
+//   return 2 * (this.height + this.width);
+// };
+
+// Rectangle.prototype.isSquare = function () {
+//   return this.height === this.width;
+// };
+
+// Rectangle.prototype.logArea = function () {
+//   console.log("Rectangle Area: " + this.area());
+// };
+
+// Object Constructor Using Class
+class Rectangle {
+  constructor(name, width, height) {
+    // console.log("Ran");
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
 }
 
-Rectangle.prototype.area = function () {
-  return this.height * this.width;
-};
-
-Rectangle.prototype.perimeter = function () {
-  return 2 * (this.height + this.width);
-};
-
-Rectangle.prototype.isSquare = function () {
-  return this.height === this.width;
-};
-
-Rectangle.prototype.logArea = function () {
-  console.log('Rectangle Area: ' + this.area());
-};
+const square = new Rectangle("Square", 20, 20);
+const square2 = new Rectangle("Square", 20, 20);
+console.log(square);
