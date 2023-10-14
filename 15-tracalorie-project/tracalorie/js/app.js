a0b7bd65f0f2890a755f33ce1b60456ae4f73fad
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 15:51:31 -0700

    Make the calories remaining and progrress bar turn red if the user goes over their daily calorie limit

--- 15-tracalorie-project/tracalorie/js/app.js
+++ 15-tracalorie-project/tracalorie/js/app.js
@@ -63,10 +63,29 @@ class CalorieTracker {
 
   _displayCaloriesRemaining() {
     const caloriesRemainingEl = document.getElementById("calories-remaining");
+    const progressEl = document.getElementById("calorie-progress");
 
     const remaining = this._calorieLimit - this._totalCalories;
 
     caloriesRemainingEl.innerHTML = remaining;
+
+    if (remaining < 0) {
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-light"
+      );
+      caloriesRemainingEl.parentElement.parentElement.classList.add(
+        "bg-danger"
+      );
+      progressEl.classList.remove("bg-success");
+      progressEl.classList.add("bg-danger");
+    } else {
+      caloriesRemainingEl.parentElement.parentElement.classList.remove(
+        "bg-danger"
+      );
+      caloriesRemainingEl.parentElement.parentElement.classList.add("bg-light");
+      progressEl.classList.remove("bg-danger");
+      progressEl.classList.add("bg-success");
+    }
   }
 
   _displayCaloriesProgress() {
