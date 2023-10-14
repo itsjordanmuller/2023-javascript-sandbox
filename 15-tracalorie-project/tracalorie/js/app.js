f5751e3af6792dd80e411c95aa3705d4aeab53cb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 16:17:59 -0700

    Copy and refactor the display new meal method to display new workouts submitted via the add workout form

--- 15-tracalorie-project/tracalorie/js/app.js
+++ 15-tracalorie-project/tracalorie/js/app.js
@@ -25,6 +25,7 @@ class CalorieTracker {
   addWorkout(workout) {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    this._displayNewWorkout(workout);
     this._render();
   }
 
@@ -121,6 +122,31 @@ class CalorieTracker {
     `;
 
     mealsEl.appendChild(mealEl);
+  }
+
+  _displayNewWorkout(workout) {
+    const workoutsEl = document.getElementById("workout-items");
+    const workoutEl = document.createElement("div");
+
+    workoutEl.classList.add("card", "my-2");
+    workoutEl.setAttribute("data-id", workout.id);
+    workoutEl.innerHTML = `
+    <div class="card-body">
+        <div class="d-flex align-items-center justify-content-between">
+        <h4 class="mx-1">${workout.name}</h4>
+        <div
+            class="fs-1 lg bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+        >
+            ${workout.calories}
+        </div>
+        <button class="delete btn btn-danger btn-sm mx-2">
+            <i class="fa-solid fa-xmark"></i>
+        </button>
+        </div>
+    </div>
+    `;
+
+    workoutsEl.appendChild(workoutEl);
   }
 
   _render() {
