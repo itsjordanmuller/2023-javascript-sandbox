85e5f1fc0f48bd4a9616d037576736a0c1851612
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-13 19:22:09 -0700

    Show how Math.PI is not writeable, configurable, or enumerable, show how to check descriptors of an object

--- 14-oop-classes-private-properties/08-flags-descriptors/script.js
+++ 14-oop-classes-private-properties/08-flags-descriptors/script.js
@@ -2,3 +2,19 @@
 // [[Enumerable]] - if `true`, the property will be returned in a `for...in` loop, otherwise not
 // [[Writable]] - if `true`, the value of the property can be changed, otherwise not
 // [[Value]] - the value of the property
+
+// PI is not Writable, Configurable, or Enumerable
+Math.PI = 4;
+console.log(Math.PI);
+
+let descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(descriptor);
+
+const rectObj = {
+  name: "Rect1",
+  width: 10,
+  height: 10,
+};
+
+descriptor = Object.getOwnPropertyDescriptor(rectObj, "name");
+console.log(descriptor);
