bf71ab91237333ab49728882e97129ea232615fc
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 18:21:17 -0700

    Make the reset day button reset all values to defaults except for daily limit, clear out DOM and arrays of all meals and workouts and their data

--- 15-tracalorie-project/tracalorie/js/app.js
+++ 15-tracalorie-project/tracalorie/js/app.js
@@ -51,6 +51,13 @@ class CalorieTracker {
     }
   }
 
+  reset() {
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+    this._render();
+  }
+
   // Private Methods //
 
   _displayCaloriesTotal() {
@@ -223,6 +230,10 @@ class App {
     document
       .getElementById("filter-workouts")
       .addEventListener("keyup", this._filterItems.bind(this, "workout"));
+
+    document
+      .getElementById("reset")
+      .addEventListener("click", this._reset.bind(this));
   }
 
   _newItem(type, e) {
@@ -281,6 +292,14 @@ class App {
         item.style.display = "none";
       }
     });
+  }
+
+  _reset() {
+    this._tracker.reset();
+    document.getElementById("meal-items").innerHTML = "";
+    document.getElementById("workout-items").innerHTML = "";
+    document.getElementById("filter-meals").value = "";
+    document.getElementById("filter-workouts").value = "";
   }
 }
 
