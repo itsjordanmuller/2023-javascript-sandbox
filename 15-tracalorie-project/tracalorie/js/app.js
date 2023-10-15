ab8afc8023d467c6c80fe571d35e858bc99600aa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-14 18:34:44 -0700

    Set calorie limit to save in local storage, get calorie limit from local storage on load

--- 15-tracalorie-project/tracalorie/js/app.js
+++ 15-tracalorie-project/tracalorie/js/app.js
@@ -1,6 +1,6 @@
 class CalorieTracker {
   constructor() {
-    this._calorieLimit = 2000;
+    this._calorieLimit = Storage.getCalorieLimit();
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
@@ -60,6 +60,7 @@ class CalorieTracker {
 
   setLimit(calorieLimit) {
     this._calorieLimit = calorieLimit;
+    Storage.setCalorieLimit(calorieLimit);
     this._displayCaloriesLimit();
     this._render();
   }
@@ -218,6 +219,10 @@ class Storage {
       calorieLimit = +localStorage.getItem("calorieLimit");
     }
     return calorieLimit;
+  }
+
+  static setCalorieLimit(calorieLimit) {
+    localStorage.setItem("calorieLimit", calorieLimit);
   }
 }
 
