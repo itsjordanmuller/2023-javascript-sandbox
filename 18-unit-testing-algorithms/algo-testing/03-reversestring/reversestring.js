c662d6ac90da8e5f63c2bbe29e955fba551fff36
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-16 15:19:55 -0700

    Show one more way a string could be reversed using split and reduce, there's many other ways this could be done

--- 18-unit-testing-algorithms/algo-testing/03-reversestring/reversestring.js
+++ 18-unit-testing-algorithms/algo-testing/03-reversestring/reversestring.js
@@ -4,14 +4,19 @@
 // }
 
 // Solution 2
+// function reverseString(string) {
+//   let reversed = "";
+
+//   for (let char of string) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Solution 3
 function reverseString(string) {
-  let reversed = "";
-
-  for (let char of string) {
-    reversed = char + reversed;
-  }
-
-  return reversed;
+  return string.split("").reduce((reversed, char) => char + reversed, "");
 }
 
 module.exports = reverseString;
