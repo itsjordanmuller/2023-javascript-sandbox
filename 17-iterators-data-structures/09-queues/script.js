e3cc6b9cf4fc1385510ceb80b179e479436d7323
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 21:44:26 -0700

    Define method to check if queue is empty and to dequeue and item from a queue

--- 17-iterators-data-structures/09-queues/script.js
+++ 17-iterators-data-structures/09-queues/script.js
@@ -9,11 +9,34 @@ class Queue {
     this._items[this._count] = item;
     this._count++;
   }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const item = this._items[this._front];
+    for (let i = this._front; i < this._count - 1; i++) {
+      this._items[i] = this._items[i + 1];
+    }
+    this._count--;
+    this._items.length = this._count;
+    return item;
+  }
+
+  isEmpty() {
+    return this._count === 0;
+  }
 }
 
 const queue = new Queue();
 
-// Queue an Item with Enqueue
+// Queue an Item to the Rear/Back/Tail with Enqueue
 queue.enqueue("Item 1");
+queue.enqueue("Item 2");
+queue.enqueue("Item 3");
+
+// Dequeue the Item in Front/Head with Dequeue
+queue.dequeue();
 
 console.log(queue);
