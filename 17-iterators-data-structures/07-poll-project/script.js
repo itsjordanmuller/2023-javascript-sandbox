c38ecc5ba6ff676754290e42a1999119636d9c51
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:33:15 -0700

    Define a function to submit the form and update the vote count in the poll Map by incrementing the value every time a vote is submitted

--- 17-iterators-data-structures/07-poll-project/script.js
+++ 17-iterators-data-structures/07-poll-project/script.js
@@ -4,3 +4,23 @@ poll.set("Vue", 0);
 poll.set("Angular", 0);
 poll.set("Svelte", 0);
 poll.set("Other", 0);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  const selectedOption = document.querySelector(
+    "input[name='poll-option']:checked"
+  );
+
+  if (!selectedOption) {
+    alert("Please select an option.");
+    return;
+  }
+
+  let voteCount = poll.get(selectedOption.value);
+  poll.set(selectedOption.value, voteCount + 1);
+
+  console.log(poll);
+}
+
+document.getElementById("poll-form").addEventListener("submit", submitForm);
