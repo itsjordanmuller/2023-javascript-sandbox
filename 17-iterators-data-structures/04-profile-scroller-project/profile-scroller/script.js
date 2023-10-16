4a5a4addb90682e8b5833dbe004a95ee7535b108
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 18:58:32 -0700

    Show a practical example of when an iterator would be used, create iterator with a generator function and test via console

--- 17-iterators-data-structures/04-profile-scroller-project/profile-scroller/script.js
+++ 17-iterators-data-structures/04-profile-scroller-project/profile-scroller/script.js
@@ -1,34 +1,48 @@
 const people = [
   {
-    name: 'Jamie Williams',
+    name: "Jamie Williams",
     age: 26,
-    gender: 'female',
-    location: 'Los Angeles, CA',
-    imageURL: 'https://randomuser.me/api/portraits/women/1.jpg',
-    looking: 'Female looking for male',
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
+    looking: "Female looking for male",
   },
   {
-    name: 'John Smith',
+    name: "John Smith",
     age: 35,
-    gender: 'male',
-    location: 'New York, NY',
-    imageURL: 'https://randomuser.me/api/portraits/men/1.jpg',
-    looking: 'Male looking for female',
+    gender: "male",
+    location: "New York, NY",
+    imageURL: "https://randomuser.me/api/portraits/men/1.jpg",
+    looking: "Male looking for female",
   },
   {
-    name: 'Bob Johnson',
+    name: "Bob Johnson",
     age: 42,
-    gender: 'male',
-    location: 'Chicago, IL',
-    imageURL: 'https://randomuser.me/api/portraits/men/2.jpg',
-    looking: 'Male looking for male',
+    gender: "male",
+    location: "Chicago, IL",
+    imageURL: "https://randomuser.me/api/portraits/men/2.jpg",
+    looking: "Male looking for male",
   },
   {
-    name: 'Shannon Jackson',
+    name: "Shannon Jackson",
     age: 29,
-    gender: 'female',
-    location: 'Los Angeles, CA',
-    imageURL: 'https://randomuser.me/api/portraits/women/2.jpg',
-    looking: 'Female looking for female',
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
+    looking: "Female looking for female",
   },
 ];
+
+const container = document.querySelector(".container");
+const img = document.querySelector("img");
+const profileInfo = document.querySelector(".profile-info");
+const nextBtn = document.getElementById("next");
+
+function* createPeopleIterator() {
+  let index = 0;
+  while (true) {
+    yield people[index++ % people.length];
+  }
+}
+
+const iterator = createPeopleIterator();
