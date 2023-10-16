076d770f2a9eb2463f1e3c6185c03fe4b958fef5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:24:26 -0700

    Create a new Map of people, set some data to it, loop through logging phones/emails to console, get keys/values/entries from Map

--- 17-iterators-data-structures/06-maps/script.js
+++ 17-iterators-data-structures/06-maps/script.js
@@ -19,3 +19,17 @@ console.log(myMap.has(3));
 // Delete a Key/Value from the Map by the Key
 myMap.delete(2);
 console.log("After Deleting Key 2:", myMap);
+
+const peopleMap = new Map();
+peopleMap.set("jordan", { phone: "555-555-5555", email: "jordan@gmail.com" });
+peopleMap.set("john", { phone: "123-456-7890", email: "john@gmail.com" });
+peopleMap.set("james", { phone: "321-654-0987", email: "james@gmail.com" });
+
+peopleMap.forEach((person) => console.log(person.phone, person.email));
+
+// Get Keys
+console.log(peopleMap.keys());
+// Get Values
+console.log(peopleMap.values());
+// Get Entries
+console.log(peopleMap.entries());
