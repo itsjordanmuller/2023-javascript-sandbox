8919a4c22684d4672f4fb6be152381d1b4f411be
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:06:44 -0700

    Delete a value from the set and log the entire set to console before and after

--- 17-iterators-data-structures/05-sets/script.js
+++ 17-iterators-data-structures/05-sets/script.js
@@ -5,6 +5,9 @@ const set1 = new Set([1, 2, 2, 3, 3, 4]);
 // Add a Number to the Set
 set.add(5);
 
-console.log(set);
+console.log("After Adding 5:", set);
 // Sets Only Store Unique Values
 console.log(set1);
+
+set.delete(5);
+console.log("After Deleting 5:", set);
