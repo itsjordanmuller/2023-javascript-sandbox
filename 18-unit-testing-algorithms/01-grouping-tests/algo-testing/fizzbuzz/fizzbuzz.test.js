178b1c78008c7a629fea15767dcee79a4fff9a31
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-16 14:44:53 -0700

    Create some more tests for the fizzBuzz function, checking if the number is returned if not divisible by 3 or 5

--- 18-unit-testing-algorithms/01-grouping-tests/algo-testing/fizzbuzz/fizzbuzz.test.js
+++ 18-unit-testing-algorithms/01-grouping-tests/algo-testing/fizzbuzz/fizzbuzz.test.js
@@ -4,4 +4,10 @@ describe("FizzBuzz", () => {
   it("should be a function", () => {
     expect(typeof fizzBuzz).toEqual("function");
   });
+
+  it("should return the number if not divisible by 3 or 5", () => {
+    expect(1).toEqual(1);
+    expect(13).toEqual(13);
+    expect(17).toEqual(17);
+  });
 });
