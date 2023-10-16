e91423ed82c3eeae8598dde62e2079277bd69dbe
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:41:53 -0700

    Set some placeholder values for each vote count, make it so the user can only submit a vote once without reloading page
    A real poll would also need backend authentication, this is just a front-end example of a map in use

--- 17-iterators-data-structures/07-poll-project/script.js
+++ 17-iterators-data-structures/07-poll-project/script.js
@@ -1,9 +1,9 @@
 const poll = new Map();
-poll.set("React", 0);
-poll.set("Vue", 0);
-poll.set("Angular", 0);
-poll.set("Svelte", 0);
-poll.set("Other", 0);
+poll.set("React", 6);
+poll.set("Vue", 5);
+poll.set("Angular", 4);
+poll.set("Svelte", 3);
+poll.set("Other", 1);
 
 function submitForm(e) {
   e.preventDefault();
@@ -21,6 +21,12 @@ function submitForm(e) {
   poll.set(selectedOption.value, voteCount + 1);
 
   displayResults();
+
+  // Disable Form Fields After Submit
+  document
+    .getElementById("poll-form")
+    .querySelectorAll("input, button")
+    .forEach((el) => el.setAttribute("disabled", true));
 }
 
 function displayResults() {
