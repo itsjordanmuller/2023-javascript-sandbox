d3ad6a9fe33e92a6094f6c5eb9b0ebb56d72ca58
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-16 15:18:17 -0700

    Another option to pass the tests for reversing a string using a for of loop

--- 18-unit-testing-algorithms/algo-testing/03-reversestring/reversestring.js
+++ 18-unit-testing-algorithms/algo-testing/03-reversestring/reversestring.js
@@ -1,5 +1,17 @@
+// Solution 1
+// function reverseString(string) {
+//   return string.split("").reverse().join("");
+// }
+
+// Solution 2
 function reverseString(string) {
-  return string.split("").reverse().join("");
+  let reversed = "";
+
+  for (let char of string) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
 }
 
 module.exports = reverseString;
