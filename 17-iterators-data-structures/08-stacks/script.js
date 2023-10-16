f6282d10e9fb85a7f947a31770edbe5ccc059c83
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 21:26:10 -0700

    Define a method to pop the item on top of the stack off the stack to remove it

--- 17-iterators-data-structures/08-stacks/script.js
+++ 17-iterators-data-structures/08-stacks/script.js
@@ -8,12 +8,31 @@ class Stack {
     this._items[this._count] = item;
     this._count++;
   }
+
+  pop() {
+    if (this._count === 0) {
+      return "Underflow";
+    }
+
+    const item = this._items[this._count - 1];
+    this._count--;
+
+    for (let i = this._count; i < this._items.length; i++) {
+      this._items[i] = this._items[i + 1];
+    }
+
+    this._items.length = this._count;
+  }
 }
 
 const stack = new Stack();
 
+// Push Items onto Stack
 stack.push("Item 1");
 stack.push("Item 2");
 stack.push("Item 3");
+
+// Pop off Item 3 (Last Item on Top of Stack)
+stack.pop();
 
 console.log(stack);
