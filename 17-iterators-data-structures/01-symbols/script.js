aafacb6e705db3b3db8bb1490b6786798567841f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 18:28:44 -0700

    Create a user object with Symbol ID as the first key, show how symbols are not enumerable by logging keys and values

--- 17-iterators-data-structures/01-symbols/script.js
+++ 17-iterators-data-structures/01-symbols/script.js
@@ -13,3 +13,21 @@ console.log(sym.description);
 
 // Returns false Symbols are Always Unique
 console.log(Symbol("sym") === Symbol("sym"));
+
+const user = {
+  [Symbol("id")]: 1,
+  name: "John Doe",
+  email: "john@gmail.com",
+};
+
+console.log(user);
+
+console.log(user[Symbol("id")]);
+
+// Symbols are NOT Enumerable
+console.log(Object.keys(user));
+console.log(Object.values(user));
+
+for (let key in user) {
+  console.log(key);
+}
