8a8b1d64e107154e13bfdcc8d52fde17c0f6ae66
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:15:14 -0700

    Create a new Map, set some keys and their values, and get those values from the map using get with the keys as the arguments

---
+++ 17-iterators-data-structures/06-maps/script.js
@@ -0,0 +1,10 @@
+const myMap = new Map();
+
+myMap.set("name", "John");
+myMap.set(1, "blue");
+myMap.set(2, "red");
+
+// Get Values from Map by Getting Keys
+console.log(myMap.get("name"));
+console.log(myMap.get(1));
+console.log(myMap.get(2));
