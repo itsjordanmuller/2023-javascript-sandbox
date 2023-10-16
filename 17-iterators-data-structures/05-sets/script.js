c0f7d72503356942f5d61db8964d29471754ea9b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-15 19:05:12 -0700

    Add a number to the set using .add method for Sets

--- 17-iterators-data-structures/05-sets/script.js
+++ 17-iterators-data-structures/05-sets/script.js
@@ -2,6 +2,9 @@ const set = new Set([1, 2, 3, 4]);
 // Duplicates Aren't Stored
 const set1 = new Set([1, 2, 2, 3, 3, 4]);
 
+// Add a Number to the Set
+set.add(5);
+
 console.log(set);
 // Sets Only Store Unique Values
 console.log(set1);
