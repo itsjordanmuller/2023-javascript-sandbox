fc4338be726cc86f10fba4c36beaa4008353c3aa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 11:38:44 -0700

    Show the promise version of using the file system to write text to a .txt file using then and catch for errors

--- 19-nodejs-modules/node-core-modules/fs.js
+++ 19-nodejs-modules/node-core-modules/fs.js
@@ -1,9 +1,15 @@
-const fs = require("fs");
+// const fs = require("fs");
+const fs = require("fs/promises");
 
 // Write to a File
 
 // Callback Version
-fs.writeFile("file1.txt", "Hello World!", (err) => {
-  if (err) throw err;
-  console.log("File Created");
-});
+// fs.writeFile("file1.txt", "Hello World!", (err) => {
+//   if (err) throw err;
+//   console.log("File Created");
+// });
+
+// Promise Version
+fs.writeFile("file2.txt", "Hello World 2!")
+  .then(() => console.log("File Created"))
+  .catch((err) => console.log(err));
