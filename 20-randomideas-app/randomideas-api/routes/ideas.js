fea869223aa1cafad07b7fd6e80d99f22d12b810
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 16:39:51 -0700

    Refactor update route to update data in the MonoDB instead of the original array of placeholder data

--- 20-randomideas-app/randomideas-api/routes/ideas.js
+++ 20-randomideas-app/randomideas-api/routes/ideas.js
@@ -68,20 +68,23 @@ router.post("/", async (req, res) => {
 });
 
 // Update an Idea Using PUT
-router.put("/:id", (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res.status(404).json({
-      success: false,
-      error: "404 Not Found - Idea with that ID not found",
-    });
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag,
+        },
+      },
+      { new: true }
+    );
+    res.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: "Something ent wrong" });
   }
-
-  idea.text = req.body.text || idea.text;
-  idea.tag = req.body.tag || idea.tag;
-
-  res.json({ success: true, data: idea });
 });
 
 // Delete an Idea Using DELETE
