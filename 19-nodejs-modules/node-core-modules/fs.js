7c75d07be7d9368ebdaba159d208221d0567630d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 11:45:40 -0700

    Show how to write to a file using async and await pattern

--- 19-nodejs-modules/node-core-modules/fs.js
+++ 19-nodejs-modules/node-core-modules/fs.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 
 // Write to a File
 
@@ -9,12 +9,24 @@ const fs = require("fs");
 // });
 
 // Promise Version
-// const fsp = require("fs/promises");
+const fsp = require("fs/promises");
 
 // fsp.writeFile("file2.txt", "Hello World 2!")
 //   .then(() => console.log("File Created"))
 //   .catch((err) => console.log(err));
 
 // Sync Version
-fs.writeFileSync("file3.txt", "Hello World 3!");
-console.log("File Created!");
+// fs.writeFileSync("file3.txt", "Hello World 3!");
+// console.log("File Created!");
+
+// Async/Await
+async function createFile(filename, content) {
+  try {
+    await fsp.writeFile(filename, content);
+    console.log("File Created!");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+createFile("file4.txt", "Hello World 4!");
