bd27a4eba4a287eb7aa8dca77497063ef078d85f
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 14:56:57 -0700

    Return JSON data using http module when user visits specific URL

--- 19-nodejs-modules/node-core-modules/http.js
+++ 19-nodejs-modules/node-core-modules/http.js
@@ -1,6 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 
+const posts = [
+  { id: 1, title: "Post One", body: "This is post 1" },
+  { id: 2, title: "Post Two", body: "This is post 2" },
+];
+
 // Create Simple Server Using http
 const server = http.createServer((request, response) => {
   const url = request.url;
@@ -19,9 +24,9 @@ const server = http.createServer((request, response) => {
     // response.writeHead(200, { "content-type": "text/html" });
     // response.writeHead(200, { "content-type": "text/plaintext" });
     // response.end("<h1>Welcome</h1>");
-  } else if (url === "/about") {
-    response.writeHead(200, { "content-type": "text/html" });
-    response.end("<h1>About</h1>");
+  } else if (url === "/api/posts") {
+    response.writeHead(200, { "content-type": "application/json" });
+    response.end(JSON.stringify({ success: true, data: posts }));
   } else {
     response.writeHead(404, { "content-type": "text/html" });
     response.end("<h1>404 - Page Not Found</h1>");
