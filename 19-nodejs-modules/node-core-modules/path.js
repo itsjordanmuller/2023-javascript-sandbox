e4c58ddfd64c22da549a112b7c0a5be1d01166d6
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 11:59:44 -0700

    Use extname method on path module to get the extension name of a filepath

--- 19-nodejs-modules/node-core-modules/path.js
+++ 19-nodejs-modules/node-core-modules/path.js
@@ -4,9 +4,15 @@ const myFilePath = "subfolder/anotherfolder/index.js";
 
 // basename()
 
-// File Name Only "index.js"
+// File Name Full - "index.js"
 const base1 = path.basename(myFilePath);
 console.log(base1);
-// File Name no Extension
+// File Name no Extension - "index"
 const base2 = path.basename(myFilePath, ".js");
 console.log(base2);
+
+// extname()
+
+// Extension Only - ".js"
+const ext = path.extname(myFilePath);
+console.log(ext);
