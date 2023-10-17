c21a579662f7f253b0b2a035350701245f37d72d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 14:46:13 -0700

    Implemented the ability to server different responses based on response codes, show a 404 error if route is not found

--- 19-nodejs-modules/node-core-modules/http.js
+++ 19-nodejs-modules/node-core-modules/http.js
@@ -4,9 +4,19 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   const url = request.url;
 
-  console.log(url);
-
-  response.end("Hello World!");
+  //   console.log(url);
+  if (url === "/") {
+    response.writeHead(200, { "content-type": "text/html" });
+    // response.writeHead(200, { "content-type": "text/plaintext" });
+    response.end("<h1>Welcome</h1>");
+  } else if (url === "/about") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.end("<h1>About</h1>");
+  } else {
+    response.writeHead(404, { "content-type": "text/html" });
+    response.end("<h1>404 - Page Not Found</h1>");
+  }
+  //   response.end("Hello World!");
 });
 
 // Listen on Port 5000
