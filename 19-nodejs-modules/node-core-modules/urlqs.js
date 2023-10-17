b9690f63e471f1e39cd1a84d37f4e5ce6c37c7e5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 14:30:01 -0700

    Use querystring module to parse and stringify URL query parameters using respective methods

--- 19-nodejs-modules/node-core-modules/urlqs.js
+++ 19-nodejs-modules/node-core-modules/urlqs.js
@@ -1,9 +1,10 @@
 const url = require("url");
+const querystring = require("querystring");
 
 // url.parse()
 // Returns a URL object with many different keys and values
 const myURL = url.parse("https://example.com/listing?id=1000&premium=true");
-console.log(myURL);
+// console.log(myURL);
 
 // url.format()
 // Returns URL as "https://www.example.com/listing?id=1000&premium=true"
@@ -16,4 +17,20 @@ const myURL2 = url.format({
     premium: true,
   },
 });
-console.log(myURL2);
+// console.log(myURL2);
+
+// querystring.parse()
+const myQs = "year=2023&month=october&day=17";
+const q = querystring.parse(myQs);
+console.log(q);
+console.log(q.year);
+console.log(q.month);
+console.log(q.day);
+
+// querystring.stringify()
+const myQs2 = querystring.stringify({
+  year: 2023,
+  month: "october",
+  day: 17,
+});
+console.log(myQs2);
