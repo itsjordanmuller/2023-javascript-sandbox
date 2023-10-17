876caca84d221627f7ead7bb83b779a9aa35b0e4
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-17 15:42:24 -0700

    Implement the ability to POST an idea to the array of ideas
    This will be replaced with a proper database later one, but for now we will store the data in memory/in the ideas array

--- 20-randomideas-app/randomideas-api/server.js
+++ 20-randomideas-app/randomideas-api/server.js
@@ -3,12 +3,17 @@ const port = 5000;
 
 const app = express();
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // API Index
 app.get("/", (req, res) => {
   // res.send("<h1>Hello World!</h1>");
   res.json({ message: "Welcome to the RandomIdeas API!" });
 });
 
+// Ideas Router from /routes/ideas.js
 const ideasRouter = require("./routes/ideas");
 app.use("/api/ideas", ideasRouter);
 
