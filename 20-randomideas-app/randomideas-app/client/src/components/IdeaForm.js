ee37bdb58459c89e775dd050e0c35ae8af371298
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-18 15:33:41 -0700

    Store the last entered username in local storage, use it to pre-populate the value for username in the form for the next submission

--- 20-randomideas-app/randomideas-app/client/src/components/IdeaForm.js
+++ 20-randomideas-app/randomideas-app/client/src/components/IdeaForm.js
@@ -23,6 +23,9 @@ class IdeaForm {
       return;
     }
 
+    // Save User to Local Storage
+    localStorage.setItem("username", this._form.elements.username.value);
+
     const idea = {
       text: this._form.elements.text.value,
       tag: this._form.elements.tag.value,
@@ -43,6 +46,8 @@ class IdeaForm {
     this._form.elements.tag.value = "";
     this._form.elements.username.value = "";
 
+    this.render();
+
     document.dispatchEvent(new Event("closemodal"));
   }
 
@@ -51,7 +56,11 @@ class IdeaForm {
         <form id="idea-form">
           <div class="form-control">
             <label for="idea-text">Enter a Username</label>
-            <input type="text" name="username" id="username" />
+            <input type="text" name="username" id="username" value="${
+              localStorage.getItem("username")
+                ? localStorage.getItem("username")
+                : ""
+            }" />
           </div>
           <div class="form-control">
             <label for="idea-text">What's Your Idea?</label>
