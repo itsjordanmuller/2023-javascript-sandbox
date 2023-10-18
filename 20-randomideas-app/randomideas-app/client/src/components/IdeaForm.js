2cf45857726bc841b5409f6ea19779fda6f91534
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-18 14:28:04 -0700

    Cut idea form from HTML, use the code in a JavaScript component, export from components folder, import to index.js

---
+++ 20-randomideas-app/randomideas-app/client/src/components/IdeaForm.js
@@ -0,0 +1,27 @@
+class IdeaForm {
+  constructor() {
+    this._formModal = document.getElementById("form-modal");
+  }
+
+  render() {
+    this._formModal.innerHTML = `
+        <form id="idea-form">
+          <div class="form-control">
+            <label for="idea-text">Enter a Username</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div class="form-control">
+            <label for="idea-text">What's Your Idea?</label>
+            <textarea name="text" id="idea-text"></textarea>
+          </div>
+          <div class="form-control">
+            <label for="tag">Tag</label>
+            <input type="text" name="tag" id="tag" />
+          </div>
+          <button class="btn" type="submit" id="submit">Submit</button>
+        </form>
+        `;
+  }
+}
+
+export default IdeaForm;
--- 20-randomideas-app/randomideas-app/client/src/index.js
+++ 20-randomideas-app/randomideas-app/client/src/index.js
@@ -1,5 +1,8 @@
 import "./css/style.css";
 import Modal from "./components/Modal";
+import IdeaForm from "./components/IdeaForm";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
