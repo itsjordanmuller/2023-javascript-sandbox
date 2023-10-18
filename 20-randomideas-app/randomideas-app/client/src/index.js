db6811f1cfcedbdadfbf87852959e4199381e10e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-18 14:14:54 -0700

    Add an event listener to the modal button to open up the window to create/submit an idea

--- 20-randomideas-app/randomideas-app/client/src/index.js
+++ 20-randomideas-app/randomideas-app/client/src/index.js
@@ -1,2 +1,11 @@
 import "./css/style.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modal-btn");
+
+function open() {
+  modal.style.display = "block";
+}
+
+modalBtn.addEventListener("click", open);
